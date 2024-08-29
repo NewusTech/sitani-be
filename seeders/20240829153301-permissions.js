664b5b19f2d637f2fe598afbeec34144e7099b9e
RozKoy <okkoy.1401@gmail.com>
2024-08-29 22:39:31 +0700

    feat(seed): permissions seeder

---
+++ seeders/20240829153301-permissions.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+        */
+        const permissions = [
+            {
+                id: 1,
+                permission_name: "ALL_CREATE",
+                description: "Can create all data",
+            },
+            {
+                id: 2,
+                permission_name: "ALL_READ",
+                description: "Can read all data",
+            },
+            {
+                id: 3,
+                permission_name: "ALL_UPDATE",
+                description: "Can update all data",
+            },
+            {
+                id: 4,
+                permission_name: "ALL_DELETE",
+                description: "Can delete all data",
+            },
+        ];
+
+        await queryInterface.bulkInsert('permissions', permissions, {});
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete('permissions', null, {});
+    }
+};
