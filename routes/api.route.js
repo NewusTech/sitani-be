460f3bc6c336ca76bc15719ecd3bb5645adc1052
RozKoy <okkoy.1401@gmail.com>
2024-08-29 14:07:27 +0700

    feat(authController): login function

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,5 +1,7 @@
+const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
 
 module.exports = function (app, urlApi) {
+    app.use(urlApi, authRoute);
     app.use(urlApi, userRoute);
 }
