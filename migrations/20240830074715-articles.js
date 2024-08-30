bea26bd76c0a73e07edaad24b0731e08ef6d7724
RozKoy <okkoy.1401@gmail.com>
2024-08-30 15:00:56 +0700

    feat(migration): articles

---
+++ migrations/20240830074715-articles.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add altering commands here.
+         *
+         * Example:
+         * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
+        await queryInterface.createTable('articles', {
+            id: {
+                type: Sequelize.BIGINT,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false,
+            },
+            judul: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            slug: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            keyword: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            excerpt: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            tag: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            alt_image: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            status: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+            },
+            image: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            konten: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            created_by: {
+                type: Sequelize.BIGINT,
+                references: {
+                    model: 'users',
+                    key: 'id'
+                }
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+        */
+        await queryInterface.dropTable('articles');
+    }
+};
