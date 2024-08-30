d250d21b3704e11c391b04b698aa2d04fbb4fb53
RozKoy <okkoy.1401@gmail.com>
2024-08-30 07:03:05 +0700

    feat(endpoint): get all roles

--- controllers/role.controller.js
+++ controllers/role.controller.js
@@ -60,4 +60,19 @@ module.exports = {
             }
         }
     },
+
+    getAll: async (req, res) => {
+        try {
+            const roles = await Role.findAll();
+
+            res.status(200).json(response(200, 'Get roles successfully', roles));
+        } catch (err) {
+            console.log(err);
+
+            logger.error(`Error : ${err}`);
+            logger.error(`Error message: ${err.message}`);
+
+            res.status(500).json(response(500, 'Internal server error'));
+        }
+    },
 }
--- routes/role.route.js
+++ routes/role.route.js
@@ -6,6 +6,7 @@ const route = express.Router();
 let prefix = '/role';
 /* -- ROUTE -- */
 route.post(prefix + '/create', roleController.create);
+route.get(prefix + '/get', roleController.getAll);
 /* -- ROUTE -- */
 
 module.exports = route;
