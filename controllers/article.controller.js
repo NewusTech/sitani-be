44d36032daea654676182ba85cc2ab08cec24a4f
RozKoy <okkoy.1401@gmail.com>
2024-08-31 13:13:13 +0700

    feat(endpoint): get one article

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -50,4 +50,35 @@ module.exports = {
 			res.status(500).json(response(500, 'Internal server error'));
 		}
 	},
+
+	getOneById: async (req, res) => {
+		try {
+			const { id } = req.params;
+
+			const article = await Article.findOne({
+				where: { id },
+				include: [
+					{
+						model: User,
+						attributes: { exclude: ['password'] },
+						as: 'User',
+					},
+				],
+			});
+
+			if (!article) {
+				res.status(404).json(response(404, 'Article not found'));
+				return;
+			}
+
+			res.status(200).json(response(200, 'Get article successfully', article));
+		} catch (err) {
+			console.log(err);
+
+			logger.error(`Error : ${err}`);
+			logger.error(`Error message: ${err.message}`);
+
+			res.status(500).json(response(500, 'Internal server error'));
+		}
+	},
 }
--- routes/article.route.js
+++ routes/article.route.js
@@ -6,6 +6,7 @@ const route = express.Router();
 let prefix = '/article';
 /* -- ROUTE -- */
 route.get(prefix + '/get', articleController.getAllWithPagination);
+route.get(prefix + '/get/:id', articleController.getOneById);
 /* -- ROUTE -- */
 
 module.exports = route;
