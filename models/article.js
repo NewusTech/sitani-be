d4247468af7d438fdcc76b14e8beae813f063761
RozKoy <okkoy.1401@gmail.com>
2024-08-31 12:17:09 +0700

    feat(model): article

---
+++ models/article.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class Article extends Model {
+        static associate(models) {
+            Article.belongsTo(models.User, {
+                foreignKey: 'createdBy'
+            });
+        }
+    }
+
+    Article.init({
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        slug: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        keyword: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        excerpt: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        tag: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        alt_image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        konten: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        createdBy: {
+            type: DataTypes.BIGINT,
+            field: 'created_by',
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            field: 'created_at',
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            field: 'updated_at',
+            allowNull: false,
+        },
+    }, {
+        tableName: 'articles',
+        modelName: 'Article',
+        sequelize,
+    });
+
+    return Article;
+};
