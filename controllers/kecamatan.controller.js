284db0a711e1a9af0cbd9ed94f1c8c6889bad7b9
RozKoy <okkoy.1401@gmail.com>
2024-09-01 14:22:47 +0700

    feat(endpoint): get all kecamatan

---
+++ controllers/kecamatan.controller.js
@@ -0,0 +1,23 @@
+const logger = require('../errorHandler/logger');
+const Validator = require("fastest-validator");
+const { response } = require('../helpers');
+const { Kecamatan } = require('../models');
+
+const v = new Validator();
+
+module.exports = {
+    getAll: async (req, res) => {
+        try {
+            const kecamatans = await Kecamatan.findAll();
+
+            res.status(200).json(response(200, 'Get kecamatan successfully', kecamatans));
+        } catch (err) {
+            console.log(err);
+
+            logger.error(`Error : ${err}`);
+            logger.error(`Error message: ${err.message}`);
+
+            res.status(500).json(response(500, 'Internal server error'));
+        }
+    },
+}
--- routes/api.route.js
+++ routes/api.route.js
@@ -1,4 +1,5 @@
 const permissionRoute = require('./permission.route');
+const kecamatanRoute = require('./kecamatan.route');
 const articleRoute = require('./article.route');
 const galeriRoute = require('./galeri.route');
 const authRoute = require('./auth.route');
@@ -7,6 +8,7 @@ const userRoute = require('./user.route');
 
 module.exports = function (app, urlApi) {
     app.use(urlApi, permissionRoute);
+    app.use(urlApi, kecamatanRoute);
     app.use(urlApi, articleRoute);
     app.use(urlApi, galeriRoute);
     app.use(urlApi, authRoute);
