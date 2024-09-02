b548ff2241f3b768a535bef3182511145b2fc9c0
RozKoy <okkoy.1401@gmail.com>
2024-09-02 17:41:48 +0700

    feat(migration): master komoditas

---
+++ migrations/20240902103920-master_komoditas.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add altering commands here.
+         *
+         * Example:
+         * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
+        await queryInterface.createTable('master_komoditas', {
+            id: {
+                type: Sequelize.BIGINT,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false,
+            },
+            nama: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            satuan: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            harga: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+        await queryInterface.dropTable('master_komoditas');
+    }
+};
