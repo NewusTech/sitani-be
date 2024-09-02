08aa8a4c4438f76f3b5aa83377fc2e9d8fd03ee6
RozKoy <okkoy.1401@gmail.com>
2024-09-02 21:31:29 +0700

    feat(endpoint): article create

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -1,13 +1,117 @@
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { generatePagination } = require('../pagination/pagination');
+const { Article, User, sequelize } = require('../models');
 const logger = require('../errorHandler/logger');
 const Validator = require("fastest-validator");
-const { Article, User } = require('../models');
 const { response } = require('../helpers');
 const { Op } = require('sequelize');
 
 const v = new Validator();
 
+const s3Client = new S3Client({
+	region: process.env.AWS_REGION,
+	useAccelerateEndpoint: true,
+	credentials: {
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	},
+});
+
 module.exports = {
+	create: async (req, res) => {
+		const transaction = await sequelize.transaction();
+
+		try {
+			const schema = {
+				judul: {
+					type: "string",
+					max: 255,
+					min: 1,
+				},
+				image: {
+					type: "string",
+					min: 1,
+				},
+				konten: {
+					type: "string",
+					max: 65000,
+					min: 1,
+				},
+			};
+
+			const { judul, konten } = req.body;
+
+			let mimetypeTemp, bufferTemp, uniqueFilenameTemp;
+			let articleObj = { konten, judul };
+
+			const files = req.files;
+			const key = 'image';
+
+			if (files[key] && files[key][0]) {
+				const file = files[key][0];
+				const { mimetype, buffer, originalname } = file;
+
+				const now = new Date();
+				const timestamp = now.toISOString().replace(/[-:.]/g, '');
+				const uniqueFilename = `${originalname.split('.')[0]}_${timestamp}`;
+
+				articleObj[key] = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.PATH_AWS}/acticles/${uniqueFilename}`;
+
+				uniqueFilenameTemp = uniqueFilename;
+				mimetypeTemp = mimetype;
+				bufferTemp = buffer;
+			}
+
+			const validate = v.validate(articleObj, schema);
+
+			if (validate.length > 0) {
+				res.status(400).json(response(400, 'Bad Request', validate));
+				return;
+			}
+
+			articleObj['excerpt'] = konten.length > 25 ? konten.substring(0, 25) : konten;
+			articleObj['slug'] = judul.toLocaleLowerCase().replaceAll(' ', '-');
+			articleObj['status'] = true;
+
+			await Article.create(articleObj);
+
+			if (uniqueFilenameTemp && bufferTemp && mimetypeTemp) {
+				const uploadParams = {
+					Bucket: process.env.AWS_S3_BUCKET,
+					Key: `${process.env.PATH_AWS}/acticles/${uniqueFilenameTemp}`,
+					Body: Buffer.from(bufferTemp),
+					ACL: 'public-read',
+					ContentType: mimetypeTemp
+				};
+				const command = new PutObjectCommand(uploadParams);
+				await s3Client.send(command);
+			}
+
+			await transaction.commit();
+
+			res.status(201).json(response(201, 'Article created'));
+		} catch (err) {
+			console.log(err);
+
+			logger.error(`Error : ${err}`);
+			logger.error(`Error message: ${err.message}`);
+
+			await transaction.rollback();
+
+			if (err.name === 'SequelizeUniqueConstraintError') {
+				res.status(400).json(response(400, 'Bad Request', [
+					{
+						type: 'duplicate',
+						message: 'Cannot created article, please use another title',
+						field: 'judul',
+					},
+				]));
+			} else {
+				res.status(500).json(response(500, 'Internal server error'));
+			}
+		}
+	},
+
 	getAllWithPagination: async (req, res) => {
 		try {
 			let { page, search, sortby } = req.query;
--- routes/article.route.js
+++ routes/article.route.js
@@ -1,10 +1,15 @@
 const articleController = require('../controllers/article.controller');
 const express = require('express');
+const multer = require('multer');
 
 const route = express.Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 let prefix = '/article';
 /* -- ROUTE -- */
+route.post(prefix + '/create', upload.fields([{ name: 'image', maxCount: 1 }]), articleController.create);
 route.get(prefix + '/get', articleController.getAllWithPagination);
 route.get(prefix + '/get/:slug', articleController.getOneBySlug);
 /* -- ROUTE -- */
