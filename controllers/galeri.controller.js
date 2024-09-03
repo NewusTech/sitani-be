46ed04f6ca0dd58fd6831e43028905cd835d02fc
RozKoy <okkoy.1401@gmail.com>
2024-09-03 09:59:54 +0700

    feat(endpoint): galeri create

--- controllers/galeri.controller.js
+++ controllers/galeri.controller.js
@@ -1,12 +1,97 @@
+const { DeleteObjectCommand, PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
 const { generatePagination } = require('../pagination/pagination');
+const { Galeri, sequelize } = require('../models');
 const logger = require('../errorHandler/logger');
 const Validator = require("fastest-validator");
 const { response } = require('../helpers');
-const { Galeri } = require('../models');
 
 const v = new Validator();
 
+const s3Client = new S3Client({
+	region: process.env.AWS_REGION,
+	useAccelerateEndpoint: true,
+	credentials: {
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	},
+});
+
 module.exports = {
+	create: async (req, res) => {
+		const transaction = await sequelize.transaction();
+
+		try {
+			const schema = {
+				deskripsi: {
+					type: "string",
+					max: 255,
+					min: 1,
+				},
+				image: {
+					type: "string",
+					min: 1,
+				},
+			};
+
+			const { deskripsi } = req.body;
+
+			let mimetypeTemp, bufferTemp, uniqueFilenameTemp;
+			let galeriObj = { deskripsi };
+
+			const files = req.files;
+			const key = 'image';
+
+			if (files[key] && files[key][0]) {
+				const file = files[key][0];
+				const { mimetype, buffer, originalname } = file;
+
+				const now = new Date();
+				const timestamp = now.toISOString().replace(/[-:.]/g, '');
+				const uniqueFilename = `${originalname.split('.')[0]}_${timestamp}`;
+
+				galeriObj[key] = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.PATH_AWS}/galeri/${uniqueFilename}`;
+
+				uniqueFilenameTemp = uniqueFilename;
+				mimetypeTemp = mimetype;
+				bufferTemp = buffer;
+			}
+
+			const validate = v.validate(galeriObj, schema);
+
+			if (validate.length > 0) {
+				res.status(400).json(response(400, 'Bad Request', validate));
+				return;
+			}
+
+			await Galeri.create(galeriObj);
+
+			if (uniqueFilenameTemp && bufferTemp && mimetypeTemp) {
+				const uploadParams = {
+					Bucket: process.env.AWS_S3_BUCKET,
+					Key: `${process.env.PATH_AWS}/galeri/${uniqueFilenameTemp}`,
+					Body: Buffer.from(bufferTemp),
+					ACL: 'public-read',
+					ContentType: mimetypeTemp
+				};
+				const command = new PutObjectCommand(uploadParams);
+				await s3Client.send(command);
+			}
+
+			await transaction.commit();
+
+			res.status(201).json(response(201, 'Galeri created'));
+		} catch (err) {
+			console.log(err);
+
+			logger.error(`Error : ${err}`);
+			logger.error(`Error message: ${err.message}`);
+
+			await transaction.rollback();
+
+			res.status(500).json(response(500, 'Internal server error'));
+		}
+	},
+
 	getAllWithPagination: async (req, res) => {
 		try {
 			let { page } = req.query;
--- routes/galeri.route.js
+++ routes/galeri.route.js
@@ -1,10 +1,15 @@
 const galeriController = require('../controllers/galeri.controller');
 const express = require('express');
+const multer = require('multer');
 
 const route = express.Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 let prefix = '/galeri';
 /* -- ROUTE -- */
+route.post(prefix + '/create', upload.fields([{ name: 'image', maxCount: 1 }]), galeriController.create);
 route.get(prefix + '/get', galeriController.getAllWithPagination);
 /* -- ROUTE -- */
 
