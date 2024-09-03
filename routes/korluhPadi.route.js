c128f69d1f9249325f9cd40e46b797a46c4049d0
RozKoy <okkoy.1401@gmail.com>
2024-09-04 01:19:31 +0700

    feat(endpoint): get one korluh padi

--- routes/korluhPadi.route.js
+++ routes/korluhPadi.route.js
@@ -7,6 +7,7 @@ let prefix = '/korluh/padi';
 /* -- ROUTE -- */
 route.post(prefix + '/create', korluhPadiController.create);
 route.get(prefix + '/get', korluhPadiController.getAll);
+route.get(prefix + '/get/one', korluhPadiController.getOne);
 /* -- ROUTE -- */
 
 module.exports = route;
