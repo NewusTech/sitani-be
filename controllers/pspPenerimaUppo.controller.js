3843d9e600d99cd20eb172f87ddbb57f129828b2
RozKoy <okkoy.1401@gmail.com>
2024-09-03 14:04:02 +0700

    feat(endpoint): psp penerima uppo update

--- controllers/pspPenerimaUppo.controller.js
+++ controllers/pspPenerimaUppo.controller.js
@@ -198,4 +198,104 @@ module.exports = {
             res.status(500).json(response(500, err.message));
         }
     },
+
+    update: async (req, res) => {
+        const transaction = await sequelize.transaction();
+
+        try {
+            const { id } = req.params;
+
+            const pspPenerimaUppo = await PspPenerimaUppo.findOne({
+                where: { id },
+            });
+
+            const schema = {
+                kecamatan_id: {
+                    type: "number",
+                    optional: true,
+                    positive: true,
+                    integer: true,
+                },
+                desa_id: {
+                    type: "number",
+                    optional: true,
+                    positive: true,
+                    integer: true,
+                },
+                nama_poktan: {
+                    type: "string",
+                    optional: true,
+                    max: 255,
+                    min: 1,
+                },
+                ketua_poktan: {
+                    type: "string",
+                    optional: true,
+                    max: 255,
+                    min: 1,
+                },
+                titik_koordinat: {
+                    type: "string",
+                    optional: true,
+                    max: 255,
+                },
+            };
+
+            const validate = v.validate(req.body, schema);
+
+            if (validate.length > 0) {
+                res.status(400).json(response(400, 'Bad Request', validate));
+                return;
+            }
+
+            if (!pspPenerimaUppo) {
+                res.status(404).json(response(404, 'Psp penerima uppo not found'));
+                return;
+            }
+
+            let { kecamatan_id, titik_koordinat, ketua_poktan, nama_poktan, desa_id } = req.body;
+
+            if (kecamatan_id) {
+                const kecamatan = await Kecamatan.findByPk(kecamatan_id);
+
+                kecamatan_id = kecamatan?.id ?? pspPenerimaUppo.kecamatanId;
+            } else {
+                kecamatan_id = pspPenerimaUppo.kecamatanId;
+            }
+
+            if (desa_id) {
+                const desa = await Desa.findByPk(desa_id);
+
+                desa_id = desa?.id ?? pspPenerimaUppo.desaId;
+            } else {
+                desa_id = pspPenerimaUppo.desaId;
+            }
+
+            titik_koordinat = titik_koordinat ?? pspPenerimaUppo.titikKoordinat;
+            ketua_poktan = ketua_poktan ?? pspPenerimaUppo.ketuaPoktan;
+            nama_poktan = nama_poktan ?? pspPenerimaUppo.namaPoktan;
+
+            await pspPenerimaUppo.update({
+                kecamatanId: kecamatan_id,
+                desaId: desa_id,
+                titikKoordinat: titik_koordinat,
+                ketuaPoktan: ketua_poktan,
+                namaPoktan: nama_poktan,
+            });
+
+            await transaction.commit();
+
+            res.status(200).json(response(200, 'Update PSP penerima uppo successfully'));
+        } catch (err) {
+            console.log(err);
+
+            logger.error(`Error : ${err}`);
+            logger.error(`Error message: ${err.message}`);
+
+            await transaction.rollback();
+
+            // res.status(500).json(response(500, 'Internal server error'));
+            res.status(500).json(response(500, err.message));
+        }
+    },
 }
--- routes/pspPenerimaUppo.route.js
+++ routes/pspPenerimaUppo.route.js
@@ -8,6 +8,7 @@ let prefix = '/penerima-uppo';
 route.post(prefix + '/create', pspPenerimaUppoController.create);
 route.get(prefix + '/get', pspPenerimaUppoController.getAll);
 route.get(prefix + '/get/:id', pspPenerimaUppoController.getOneById);
+route.put(prefix + '/update/:id', pspPenerimaUppoController.update);
 /* -- ROUTE -- */
 
 module.exports = route;
