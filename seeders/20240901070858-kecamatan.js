2f7c4ac0b375ac4f97e17a1b9d7758bb06a4c9fa
syukron <syukron488@gmail.com>
2024-09-04 09:36:38 +0700

    add seeder kecamatan

--- seeders/20240901070858-kecamatan.js
+++ seeders/20240901070858-kecamatan.js
@@ -13,36 +13,34 @@ module.exports = {
          * }], {});
         */
         const kecamatan = [
-            {
-                id: 1,
-                nama: "Kecamatan A",
-                created_at: new Date(),
-                updated_at: new Date(),
-            },
-            {
-                id: 2,
-                nama: "Kecamatan B",
-                created_at: new Date(),
-                updated_at: new Date(),
-            },
-            {
-                id: 3,
-                nama: "Kecamatan C",
-                created_at: new Date(),
-                updated_at: new Date(),
-            },
-            {
-                id: 4,
-                nama: "Kecamatan D",
-                created_at: new Date(),
-                updated_at: new Date(),
-            },
-            {
-                id: 5,
-                nama: "Kecamatan E",
-                created_at: new Date(),
-                updated_at: new Date(),
-            },
+            { nama: 'Metro Kibang', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Batanghari', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Sekampung', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Margatiga', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Sekampung Udik', created_at: new Date(), updated_at: new Date() },
+
+            { nama: 'Jabung', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Pasir Sakti', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Waway Karya', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Marga Sekampung', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Labuhan Maringgai', created_at: new Date(), updated_at: new Date() },
+
+            { nama: 'Mataram Baru', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Bandar Sribawono', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Melinting', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Gunung Pelindung', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Way Jepara', created_at: new Date(), updated_at: new Date() },
+
+            { nama: 'Braja Slebah', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Labuhan Ratu', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Sukadana', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Bumi Agung', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Batanghari Nuban', created_at: new Date(), updated_at: new Date() },
+            
+            { nama: 'Pekalongan', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Raman Utara', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Purbolinggo', created_at: new Date(), updated_at: new Date() },
+            { nama: 'Way Bungur', created_at: new Date(), updated_at: new Date() },
         ];
 
         await queryInterface.bulkInsert({ tableName: "master_kecamatan" }, kecamatan, {});
