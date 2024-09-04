56256865e382ac15616f9ef798c48bac86653be5
RozKoy <okkoy.1401@gmail.com>
2024-09-04 08:59:48 +0700

    feat(endpoint): korluh padi delete

--- controllers/korluhPadi.controller.js
+++ controllers/korluhPadi.controller.js
@@ -667,4 +667,37 @@ module.exports = {
             res.status(500).json(response(500, err.message));
         }
     },
+
+    delete: async (req, res) => {
+        const transaction = await sequelize.transaction();
+
+        try {
+            const { id } = req.params;
+
+            const korluhPadi = await KorluhPadi.findOne({
+                where: { id },
+            });
+
+            if (!korluhPadi) {
+                res.status(404).json(response(404, 'Korluh padi not found'));
+                return;
+            }
+
+            await korluhPadi.destroy();
+
+            await transaction.commit();
+
+            res.status(200).json(response(200, 'Delete korluh padi successfully'));
+        } catch (err) {
+            console.log(err);
+
+            logger.error(`Error : ${err}`);
+            logger.error(`Error message: ${err.message}`);
+
+            await transaction.rollback();
+
+            // res.status(500).json(response(500, 'Internal server error'));
+            res.status(500).json(response(500, err.message));
+        }
+    },
 }
