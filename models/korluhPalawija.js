9108499d0de4c22792a282368ef9c8c5a31cd702
RozKoy <okkoy.1401@gmail.com>
2024-09-06 06:24:29 +0700

    feat(model): korluh palawija

---
+++ models/korluhPalawija.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class KorluhPalawija extends Model {
+        static associate(models) {
+            KorluhPalawija.belongsTo(models.Kecamatan, {
+                foreignKey: 'kecamatanId',
+                as: 'kecamatan',
+            });
+            KorluhPalawija.belongsTo(models.Desa, {
+                foreignKey: 'desaId',
+                as: 'desa'
+            });
+        }
+    }
+
+    KorluhPalawija.init({
+        kecamatanId: {
+            type: DataTypes.BIGINT,
+            field: 'kecamatan_id',
+        },
+        desaId: {
+            type: DataTypes.BIGINT,
+            field: 'desa_id',
+        },
+        tanggal: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            field: 'created_at',
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            field: 'updated_at',
+            allowNull: false,
+        },
+    }, {
+        tableName: 'korluh_palawija',
+        modelName: 'KorluhPalawija',
+        sequelize,
+    });
+
+    return KorluhPalawija;
+};
