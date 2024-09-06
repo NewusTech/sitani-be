c3ce04290bc127cec4f8bb784dd8a9a86c68cd63
RozKoy <okkoy.1401@gmail.com>
2024-09-06 22:25:44 +0700

    feat(endpoint): get all korluh tanaman biofarmaka

--- routes/korluhTanamanBiofarmaka.route.js
+++ routes/korluhTanamanBiofarmaka.route.js
@@ -6,6 +6,7 @@ const route = express.Router();
 let prefix = '/korluh/tanaman-biofarmaka';
 /* -- ROUTE -- */
 route.post(prefix + '/create', korluhTanamanBiofarmakaController.create);
+route.get(prefix + '/get', korluhTanamanBiofarmakaController.getAll);
 /* -- ROUTE -- */
 
 module.exports = route;
