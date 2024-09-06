b15612aa0a8ba55699f62f4a3e0470df7f9454f0
RozKoy <okkoy.1401@gmail.com>
2024-09-06 21:23:15 +0700

    feat(endpoint): get all korluh tanaman hias

--- routes/korluhTanamanHias.route.js
+++ routes/korluhTanamanHias.route.js
@@ -6,6 +6,7 @@ const route = express.Router();
 let prefix = '/korluh/tanaman-hias';
 /* -- ROUTE -- */
 route.post(prefix + '/create', korluhTanamanHiasController.create);
+route.get(prefix + '/get', korluhTanamanHiasController.getAll);
 /* -- ROUTE -- */
 
 module.exports = route;
