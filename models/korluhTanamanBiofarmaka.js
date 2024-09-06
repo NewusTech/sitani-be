5b05aa51fdd12f4bc377d6545c479b5adcb18168
RozKoy <okkoy.1401@gmail.com>
2024-09-06 22:13:31 +0700

    feat(model): korluh tanaman biofarmaka

---
+++ models/korluhTanamanBiofarmaka.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class KorluhTanamanBiofarmaka extends Model {
+        static associate(models) {
+            KorluhTanamanBiofarmaka.belongsTo(models.Kecamatan, {
+                foreignKey: 'kecamatanId',
+                as: 'kecamatan',
+            });
+            KorluhTanamanBiofarmaka.belongsTo(models.Desa, {
+                foreignKey: 'desaId',
+                as: 'desa'
+            });
+        }
+    }
+
+    KorluhTanamanBiofarmaka.init({
+        kecamatanId: {
+            type: DataTypes.BIGINT,
+            field: 'kecamatan_id',
+        },
+        desaId: {
+            type: DataTypes.BIGINT,
+            field: 'desa_id',
+        },
+        tanggal: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            field: 'created_at',
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            field: 'updated_at',
+            allowNull: false,
+        },
+    }, {
+        tableName: 'korluh_tanaman_biofarmaka',
+        modelName: 'KorluhTanamanBiofarmaka',
+        sequelize,
+    });
+
+    return KorluhTanamanBiofarmaka;
+};
