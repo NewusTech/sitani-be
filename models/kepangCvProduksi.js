45f29dc36e093f98b2f42efdaff168c2825f7195
RozKoy <okkoy.1401@gmail.com>
2024-09-08 22:51:59 +0700

    feat(model): kepang cv produksi

---
+++ models/kepangCvProduksi.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class KepangCvProduksi extends Model {
+        static associate(models) {
+        }
+    }
+
+    KepangCvProduksi.init({
+        bulan: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+
+        panen: {
+            type: DataTypes.DOUBLE,
+        },
+        gkpTkPetani: {
+            type: DataTypes.INTEGER,
+            field: 'gkp_tk_petani'
+        },
+        gkpTkPenggilingan: {
+            type: DataTypes.INTEGER,
+            field: 'gkp_tk_penggilingan'
+        },
+        jpk: {
+            type: DataTypes.INTEGER,
+        },
+        cabaiMerahKeriting: {
+            type: DataTypes.INTEGER,
+            field: 'cabai_merah_keriting',
+        },
+        berasMedium: {
+            type: DataTypes.INTEGER,
+            field: 'beras_medium'
+        },
+        berasPremium: {
+            type: DataTypes.INTEGER,
+            field: 'beras_premium',
+        },
+        stokGkg: {
+            type: DataTypes.INTEGER,
+            field: 'stok_gkg'
+        },
+        stokBeras: {
+            type: DataTypes.INTEGER,
+            field: 'stok_beras',
+        },
+
+        createdAt: {
+            type: DataTypes.DATE,
+            field: 'created_at',
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            field: 'updated_at',
+            allowNull: false,
+        },
+    }, {
+        tableName: 'kepang_cv_produksi',
+        modelName: 'KepangCvProduksi',
+        sequelize,
+    });
+
+    return KepangCvProduksi;
+};
