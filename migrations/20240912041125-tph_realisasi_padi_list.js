200a8d5289c7fa280abff384d6be78263155b850
RozKoy <okkoy.1401@gmail.com>
2024-09-12 11:23:03 +0700

    feat(migrations): tph realisasi padi n tph realisasi padi list

--- migrations/20240912041125-tph_realisasi_padi_list.js
+++ migrations/20240912041125-tph_realisasi_padi_list.js
@@ -9,12 +9,19 @@ module.exports = {
          * Example:
          * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
          */
-        await queryInterface.createTable('realisasi_panen_padi', {
+        await queryInterface.createTable('tph_realisasi_padi_list', {
             id: {
                 type: Sequelize.BIGINT,
                 autoIncrement: true,
                 primaryKey: true,
                 allowNull: false,
+            },
+            tph_realisasi_padi_id: {
+                type: Sequelize.BIGINT,
+                references: {
+                    model: 'tph_realisasi_padi',
+                    key: 'id'
+                }
             },
             kecamatan_id: {
                 type: Sequelize.BIGINT,
@@ -23,20 +30,14 @@ module.exports = {
                     key: 'id'
                 }
             },
-            desa_id: {
-                type: Sequelize.BIGINT,
-                references: {
-                    model: 'master_desa',
-                    key: 'id'
-                }
-            },
-            panen_sawah: {
+
+            panen_lahan_sawah: {
                 type: Sequelize.DOUBLE,
             },
-            produktivitas_sawah: {
+            produktivitas_lahan_sawah: {
                 type: Sequelize.DOUBLE,
             },
-            produksi_sawah: {
+            produksi_lahan_sawah: {
                 type: Sequelize.DOUBLE,
             },
             panen_lahan_kering: {
@@ -57,6 +58,7 @@ module.exports = {
             produksi_total: {
                 type: Sequelize.DOUBLE,
             },
+
             created_at: {
                 type: Sequelize.DATE,
                 allowNull: false,
@@ -74,7 +76,7 @@ module.exports = {
          *
          * Example:
          * await queryInterface.dropTable('users');
-        */
-        await queryInterface.dropTable('realisasi_panen_padi');
+         */
+        await queryInterface.dropTable('tph_realisasi_padi_list');
     }
 };
