61f3b5b63bb37c4129a92b25be5e6f723ae04474
RozKoy <okkoy.1401@gmail.com>
2024-09-14 06:26:53 +0700

    feat(model): validasi korluh padi

---
+++ models/validasiKorluhPadi.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class ValidasiKorluhPadi extends Model {
+        static associate(models) {
+            ValidasiKorluhPadi.belongsTo(models.Kecamatan, {
+                foreignKey: 'kecamatanId',
+                as: 'kecamatan'
+            });
+        }
+    }
+
+    ValidasiKorluhPadi.init({
+        kecamatanId: {
+            type: DataTypes.BIGINT,
+            field: 'kecamatan_id'
+        },
+        bulan: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
+        statusTkKecamatan: {
+            type: DataTypes.ENUM('proses', 'tolak', 'terima'),
+            field: 'status_tk_kecamatan',
+            defaultValue: 'proses',
+            allowNull: false,
+        },
+        statusTkKabupaten: {
+            type: DataTypes.ENUM('proses', 'tolak', 'terima'),
+            field: 'status_tk_kabupaten',
+            defaultValue: 'proses',
+            allowNull: false,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            field: 'created_at',
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            field: 'updated_at',
+            allowNull: false,
+        },
+    }, {
+        tableName: 'validasi_korluh_padi',
+        modelName: 'ValidasiKorluhPadi',
+        sequelize,
+    });
+
+    return ValidasiKorluhPadi;
+};
