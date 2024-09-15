4253fffb47d254367d758990e8e39994ce8c4f8b
RozKoy <okkoy.1401@gmail.com>
2024-09-15 12:39:35 +0700

    feat(models): validasi korluh sayur dan buah n validator korluh sayur dan buah

---
+++ models/validasiKorluhSayurBuah.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class ValidasiKorluhSayurBuah extends Model {
+        static associate(models) {
+            ValidasiKorluhSayurBuah.belongsTo(models.Kecamatan, {
+                foreignKey: 'kecamatanId',
+                as: 'kecamatan'
+            });
+        }
+    }
+
+    ValidasiKorluhSayurBuah.init({
+        kecamatanId: {
+            type: DataTypes.BIGINT,
+            field: 'kecamatan_id'
+        },
+        bulan: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
+        statusTkKecamatan: {
+            type: DataTypes.ENUM('proses', 'tolak', 'terima'),
+            field: 'status_tk_kecamatan',
+            defaultValue: 'proses',
+            allowNull: false,
+        },
+        statusTkKabupaten: {
+            type: DataTypes.ENUM('proses', 'tolak', 'terima'),
+            field: 'status_tk_kabupaten',
+            defaultValue: 'proses',
+            allowNull: false,
+        },
+        keterangan: {
+            type: DataTypes.STRING,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            field: 'created_at',
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            field: 'updated_at',
+            allowNull: false,
+        },
+    }, {
+        tableName: 'validasi_korluh_sayur_dan_buah',
+        modelName: 'ValidasiKorluhSayurBuah',
+        sequelize,
+    });
+
+    return ValidasiKorluhSayurBuah;
+};
