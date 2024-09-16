2301b913c76e3034ff1b57bdea415d3c6780a208
RozKoy <okkoy.1401@gmail.com>
2024-09-16 11:45:54 +0700

    feat(endpoint): download perkebunan kecamatan n kabupaten (without sum all)

--- routes/download.route.js
+++ routes/download.route.js
@@ -1,3 +1,4 @@
+const perkebunanDownloadController = require('../controllers/perkebunanDownload.controller');
 const penyuluhDownloadController = require('../controllers/penyuluhDownload.controller');
 const express = require('express');
 
@@ -5,6 +6,8 @@ const route = express.Router();
 
 let prefix = '/download';
 /* -- ROUTE -- */
+route.get(prefix + '/perkebunan-kabupaten', perkebunanDownloadController.kabupaten);
+route.get(prefix + '/perkebunan-kecamatan', perkebunanDownloadController.kecamatan);
 route.get(prefix + '/penyuluh-kabupaten', penyuluhDownloadController.kabupaten);
 route.get(prefix + '/penyuluh-kecamatan', penyuluhDownloadController.kecamatan);
 /* -- ROUTE -- */
