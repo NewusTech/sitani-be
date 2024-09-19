ae7886b7e680699397a9ace904a47ac4e4d01253
RozKoy <okkoy.1401@gmail.com>
2024-09-19 13:07:26 +0700

    feat(endpoint): get year list in korluh tanaman hias

--- controllers/korluhMasterTahun.controller.js
+++ controllers/korluhMasterTahun.controller.js
@@ -1,4 +1,4 @@
-const { KorluhTanamanBiofarmaka, KorluhSayurBuah, KorluhPalawija, KorluhPadi, sequelize } = require('../models');
+const { KorluhTanamanBiofarmaka, KorluhTanamanHias, KorluhSayurBuah, KorluhPalawija, KorluhPadi, sequelize } = require('../models');
 const logger = require('../errorHandler/logger');
 const Validator = require("fastest-validator");
 const { response } = require('../helpers');
@@ -92,6 +92,30 @@ module.exports = {
             tahun = tahun.map(item => item.tahun);
 
             res.status(200).json(response(200, 'Get tahun korluh tanaman biofarmaka successfully', tahun));
+        } catch (err) {
+            console.log(err);
+
+            logger.error(`Error : ${err}`);
+            logger.error(`Error message: ${err.message}`);
+
+            // res.status(500).json(response(500, 'Internal server error'));
+            res.status(500).json(response(500, err.message));
+        }
+    },
+
+    tanamanHias: async (req, res) => {
+        try {
+            let tahun = await KorluhTanamanHias.findAll({
+                attributes: [
+                    [sequelize.fn('DISTINCT', sequelize.fn('YEAR', sequelize.col('tanggal'))), 'tahun'],
+                ],
+                order: [['tahun', 'DESC']],
+                raw: true,
+            });
+
+            tahun = tahun.map(item => item.tahun);
+
+            res.status(200).json(response(200, 'Get tahun korluh tanaman hias successfully', tahun));
         } catch (err) {
             console.log(err);
 
--- routes/korluhMasterTahun.route.js
+++ routes/korluhMasterTahun.route.js
@@ -5,10 +5,11 @@ const route = express.Router();
 
 let prefix = '/korluh/master-tahun';
 /* -- ROUTE -- */
-route.get(prefix + '/padi', korluhMasterTahunController.padi);
-route.get(prefix + '/palawija', korluhMasterTahunController.palawija);
-route.get(prefix + '/sayur-buah', korluhMasterTahunController.sayurBuah);
 route.get(prefix + '/tanaman-biofarmaka', korluhMasterTahunController.tanamanBiofarmaka);
+route.get(prefix + '/tanaman-hias', korluhMasterTahunController.tanamanHias);
+route.get(prefix + '/sayur-buah', korluhMasterTahunController.sayurBuah);
+route.get(prefix + '/palawija', korluhMasterTahunController.palawija);
+route.get(prefix + '/padi', korluhMasterTahunController.padi);
 /* -- ROUTE -- */
 
 module.exports = route;
