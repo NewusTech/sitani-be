fd10f1bd893b80133d2d8b2b683776eadf1353a6
RozKoy <okkoy.1401@gmail.com>
2024-09-19 13:23:52 +0700

    feat(endpoint): get year list in tph realisasi palawija 2

--- controllers/tphMasterTahun.controller.js
+++ controllers/tphMasterTahun.controller.js
@@ -1,4 +1,4 @@
-const { TphRealisasiPalawija1, TphLahanBukanSawah, TphRealisasiPadi, TphLahanSawah, sequelize } = require('../models');
+const { TphRealisasiPalawija1, TphRealisasiPalawija2, TphLahanBukanSawah, TphRealisasiPadi, TphLahanSawah, sequelize } = require('../models');
 const logger = require('../errorHandler/logger');
 const Validator = require("fastest-validator");
 const { response } = require('../helpers');
@@ -84,6 +84,30 @@ module.exports = {
             tahun = tahun.map(item => item.tahun);
 
             res.status(200).json(response(200, 'Get tahun tph realisasi palawija 1 successfully', tahun));
+        } catch (err) {
+            console.log(err);
+
+            logger.error(`Error : ${err}`);
+            logger.error(`Error message: ${err.message}`);
+
+            // res.status(500).json(response(500, 'Internal server error'));
+            res.status(500).json(response(500, err.message));
+        }
+    },
+
+    realisasiPalawija2: async (req, res) => {
+        try {
+            let tahun = await TphRealisasiPalawija2.findAll({
+                attributes: [
+                    [sequelize.fn('DISTINCT', sequelize.fn('YEAR', sequelize.col('bulan'))), 'tahun'],
+                ],
+                order: [['tahun', 'DESC']],
+                raw: true,
+            });
+
+            tahun = tahun.map(item => item.tahun);
+
+            res.status(200).json(response(200, 'Get tahun tph realisasi palawija 2 successfully', tahun));
         } catch (err) {
             console.log(err);
 
--- routes/tphMasterTahun.route.js
+++ routes/tphMasterTahun.route.js
@@ -6,6 +6,7 @@ const route = express.Router();
 let prefix = '/tph/master-tahun';
 /* -- ROUTE -- */
 route.get(prefix + '/realisasi-palawija-1', tphMasterTahunController.realisasiPalawija1);
+route.get(prefix + '/realisasi-palawija-2', tphMasterTahunController.realisasiPalawija2);
 route.get(prefix + '/lahan-bukan-sawah', tphMasterTahunController.lahanBukanSawah);
 route.get(prefix + '/realisasi-padi', tphMasterTahunController.realisasiPadi);
 route.get(prefix + '/lahan-sawah', tphMasterTahunController.lahanSawah);
