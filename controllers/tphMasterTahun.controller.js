2f3f4452828f59ad5c54c40f3d279de631d277b0
RozKoy <okkoy.1401@gmail.com>
2024-09-19 13:15:46 +0700

    feat(endpoint): get year list in tph lahan sawah

--- controllers/tphMasterTahun.controller.js
+++ controllers/tphMasterTahun.controller.js
@@ -1,4 +1,4 @@
-const { TphLahanBukanSawah, sequelize } = require('../models');
+const { TphLahanBukanSawah, TphLahanSawah, sequelize } = require('../models');
 const logger = require('../errorHandler/logger');
 const Validator = require("fastest-validator");
 const { response } = require('../helpers');
@@ -16,6 +16,26 @@ module.exports = {
             tahun = tahun.map(item => item.tahun);
 
             res.status(200).json(response(200, 'Get tahun tph lahan bukan sawah successfully', tahun));
+        } catch (err) {
+            console.log(err);
+
+            logger.error(`Error : ${err}`);
+            logger.error(`Error message: ${err.message}`);
+
+            // res.status(500).json(response(500, 'Internal server error'));
+            res.status(500).json(response(500, err.message));
+        }
+    },
+
+    lahanSawah: async (req, res) => {
+        try {
+            let tahun = await TphLahanSawah.findAll({
+                order: [['tahun', 'DESC']],
+            });
+
+            tahun = tahun.map(item => item.tahun);
+
+            res.status(200).json(response(200, 'Get tahun tph lahan sawah successfully', tahun));
         } catch (err) {
             console.log(err);
 
--- routes/tphMasterTahun.route.js
+++ routes/tphMasterTahun.route.js
@@ -6,6 +6,7 @@ const route = express.Router();
 let prefix = '/tph/master-tahun';
 /* -- ROUTE -- */
 route.get(prefix + '/lahan-bukan-sawah', tphMasterTahunController.lahanBukanSawah);
+route.get(prefix + '/lahan-sawah', tphMasterTahunController.lahanSawah);
 /* -- ROUTE -- */
 
 module.exports = route;
