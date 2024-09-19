b6f28c73053f0a9f2d32796b262fedb7dcfa3ce3
RozKoy <okkoy.1401@gmail.com>
2024-09-19 13:03:19 +0700

    feat(endpoint): get year list in korluh sayur dan buah

--- controllers/korluhMasterTahun.controller.js
+++ controllers/korluhMasterTahun.controller.js
@@ -1,4 +1,4 @@
-const { KorluhPalawija, KorluhPadi, sequelize } = require('../models');
+const { KorluhSayurBuah, KorluhPalawija, KorluhPadi, sequelize } = require('../models');
 const logger = require('../errorHandler/logger');
 const Validator = require("fastest-validator");
 const { response } = require('../helpers');
@@ -44,6 +44,30 @@ module.exports = {
             tahun = tahun.map(item => item.tahun);
 
             res.status(200).json(response(200, 'Get tahun korluh palawija successfully', tahun));
+        } catch (err) {
+            console.log(err);
+
+            logger.error(`Error : ${err}`);
+            logger.error(`Error message: ${err.message}`);
+
+            // res.status(500).json(response(500, 'Internal server error'));
+            res.status(500).json(response(500, err.message));
+        }
+    },
+
+    sayurBuah: async (req, res) => {
+        try {
+            let tahun = await KorluhSayurBuah.findAll({
+                attributes: [
+                    [sequelize.fn('DISTINCT', sequelize.fn('YEAR', sequelize.col('tanggal'))), 'tahun'],
+                ],
+                order: [['tahun', 'DESC']],
+                raw: true,
+            });
+
+            tahun = tahun.map(item => item.tahun);
+
+            res.status(200).json(response(200, 'Get tahun korluh sayur dan buah successfully', tahun));
         } catch (err) {
             console.log(err);
 
--- routes/korluhMasterTahun.route.js
+++ routes/korluhMasterTahun.route.js
@@ -7,6 +7,7 @@ let prefix = '/korluh/master-tahun';
 /* -- ROUTE -- */
 route.get(prefix + '/padi', korluhMasterTahunController.padi);
 route.get(prefix + '/palawija', korluhMasterTahunController.palawija);
+route.get(prefix + '/sayur-buah', korluhMasterTahunController.sayurBuah);
 /* -- ROUTE -- */
 
 module.exports = route;
