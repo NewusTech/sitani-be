48de600853ad715c49a52ad319a72dbe9e8c4290
RozKoy <okkoy.1401@gmail.com>
2024-09-21 01:17:47 +0700

    fix(number fixed helper): logical

--- helpers/number.fixed.js
+++ helpers/number.fixed.js
@@ -2,7 +2,15 @@ module.exports = {
     fixedNumber: (obj, fix = 2) => {
         let temp = {}
         for (let i of Object.keys(obj)) {
-            temp[i] = obj[i] ? (Number.isInteger(obj[i]) ? obj[i] : (obj[i]).toFixed(fix)) : null;
+            temp[i] = obj[i];
+            if (!isNaN(Number(obj[i]))) {
+                const num = Number(obj[i]);
+                if (Number.isInteger(num)) {
+                    temp[i] = num;
+                } else {
+                    temp[i] = num.toFixed(fix);
+                }
+            }
         }
         return temp;
     }
