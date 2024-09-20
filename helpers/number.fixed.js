c2b422ecea0327d07c45b3b49506d4993918ffa3
RozKoy <okkoy.1401@gmail.com>
2024-09-20 23:52:17 +0700

    feat(number fixed helper): not fixed integer number

--- helpers/number.fixed.js
+++ helpers/number.fixed.js
@@ -2,7 +2,7 @@ module.exports = {
     fixedNumber: (obj, fix = 2) => {
         let temp = {}
         for (let i of Object.keys(obj)) {
-            temp[i] = obj[i] ? (obj[i]).toFixed(fix) : null;
+            temp[i] = obj[i] ? (Number.isInteger(obj[i]) ? obj[i] : (obj[i]).toFixed(fix)) : null;
         }
         return temp;
     }
