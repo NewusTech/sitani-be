cc1dea6ce54be0ae5dc231de3aeaff878ae722cc
RozKoy <okkoy.1401@gmail.com>
2024-09-20 23:35:49 +0700

    feat(helper): number fixed

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,6 @@
 const { getFirstLastDate, dateGenerate } = require('./date.generator');
 const { jwtGenerate } = require('./token.generator');
 const { response } = require('./response.formatter');
+const { fixedNumber } = require('./number.fixed');
 
-module.exports = { getFirstLastDate, dateGenerate, jwtGenerate, response };
+module.exports = { getFirstLastDate, dateGenerate, jwtGenerate, fixedNumber, response };
---
+++ helpers/number.fixed.js
@@ -0,0 +1,9 @@
+module.exports = {
+    fixedNumber: (obj, fix = 2) => {
+        let temp = {}
+        for (let i of Object.keys(obj)) {
+            temp[i] = obj[i] ? (obj[i]).toFixed(fix) : null;
+        }
+        return temp;
+    }
+}
