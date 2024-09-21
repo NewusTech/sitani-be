56f30111db00a1f851180a37c5fc1e1214333ba0
RozKoy <okkoy.1401@gmail.com>
2024-09-21 12:17:25 +0700

    feat(role permission seeder): add data

--- seeders/20240906182433-role_permissions.js
+++ seeders/20240906182433-role_permissions.js
@@ -12,12 +12,29 @@ module.exports = {
          *   isBetaMember: false
          * }], {});
         */
-        const dataList = [
-            {
-                role_id: 1,
-                permission_id: 1,
-            },
+        let dataList = [];
+        let mapRole = [
+            { role: 1, min: 1, max: 1 },
+            { role: 2, min: 2, max: 13 },
+            { role: 3, min: 14, max: 33 },
+            { role: 4, min: 34, max: 53 },
+            { role: 5, min: 54, max: 61 },
+            { role: 6, min: 62, max: 69 },
+            { role: 7, min: 70, max: 81 },
+            { role: 8, min: 82, max: 85 },
+            { role: 9, min: 86, max: 105 },
+            { role: 10, min: 106, max: 125 },
+            { role: 11, min: 126, max: 145 },
         ];
+
+        mapRole.forEach(item => {
+            for (let i = item.min; i <= item.max; i++) {
+                dataList.push({
+                    role_id: item.role,
+                    permission_id: i,
+                });
+            }
+        });
 
         await queryInterface.bulkInsert({ tableName: "role_permissions" }, dataList, {});
     },
