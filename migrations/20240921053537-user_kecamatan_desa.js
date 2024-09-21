21add9afc17f8c60cf3d0ef56bb3c1a7fbbff489
RozKoy <okkoy.1401@gmail.com>
2024-09-21 12:39:35 +0700

    feat(migration): user kecamatan desa

---
+++ migrations/20240921053537-user_kecamatan_desa.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add altering commands here.
+         *
+         * Example:
+         * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
+        await queryInterface.createTable('user_kecamatan_desa', {
+            user_id: {
+                type: Sequelize.BIGINT,
+                primaryKey: true,
+                references: {
+                    model: 'users',
+                    key: 'id'
+                }
+            },
+            kecamatan_id: {
+                type: Sequelize.BIGINT,
+                references: {
+                    model: 'master_kecamatan',
+                    key: 'id'
+                }
+            },
+            desa_id: {
+                type: Sequelize.BIGINT,
+                references: {
+                    model: 'master_desa',
+                    key: 'id'
+                }
+            },
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+        */
+        await queryInterface.dropTable('user_kecamatan_desa');
+    }
+};
