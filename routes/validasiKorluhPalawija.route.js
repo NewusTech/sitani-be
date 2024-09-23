bb00e41d35d84e9f2ba75b46dfd88aae709b3683
RozKoy <okkoy.14@gmail.com>
2024-09-24 04:24:07 +0700

    feat(validasi korluh palawija): kec data with middleware

--- routes/validasiKorluhPalawija.route.js
+++ routes/validasiKorluhPalawija.route.js
@@ -1,4 +1,5 @@
 const validasiKorluhPalawijaController = require('../controllers/validasiKorluhPalawija.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
@@ -7,7 +8,7 @@ let prefix = '/validasi/korluh-palawija';
 /* -- ROUTE -- */
 route.post(prefix + '/kec', validasiKorluhPalawijaController.kecVal);
 route.post(prefix + '/kab', validasiKorluhPalawijaController.kabVal);
-route.get(prefix + '/kec', validasiKorluhPalawijaController.kecData);
+route.get(prefix + '/kec', [mid.checkUserOrPass()], validasiKorluhPalawijaController.kecData);
 route.get(prefix + '/kab', validasiKorluhPalawijaController.kabData);
 /* -- ROUTE -- */
 
