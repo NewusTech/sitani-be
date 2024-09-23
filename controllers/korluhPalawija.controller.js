0c45a579cc547709f1205524096dd5bc3a209265
RozKoy <okkoy.14@gmail.com>
2024-09-24 03:15:13 +0700

    feat(korluh palawija): get all with middleware

--- controllers/korluhPalawija.controller.js
+++ controllers/korluhPalawija.controller.js
@@ -1,4 +1,4 @@
-const { ValidasiKorluhPalawija, KorluhMasterPalawija, KorluhPalawijaList, KorluhPalawija, Kecamatan, Desa, sequelize } = require('../models');
+const { ValidasiKorluhPalawija, KorluhMasterPalawija, KorluhPalawijaList, KorluhPalawija, Kecamatan, Desa, User, sequelize } = require('../models');
 const { dateGenerate, response, fixedNumber } = require('../helpers');
 const { generatePagination } = require('../pagination/pagination');
 const logger = require('../errorHandler/logger');
@@ -395,6 +395,28 @@ module.exports = {
             page = isNaN(parseInt(page)) ? 1 : parseInt(page);
 
             const offset = (page - 1) * limit;
+
+            if (req?.root?.userId) {
+                const user = await User.findByPk(req.root.userId, {
+                    include: [
+                        {
+                            model: Kecamatan,
+                            as: 'kecamatans'
+                        },
+                        {
+                            model: Desa,
+                            as: 'desas'
+                        },
+                    ]
+                });
+
+                if (user?.kecamatans?.length) {
+                    kecamatan = user.kecamatans[0].id;
+                }
+                if (user?.desas?.length) {
+                    desa = user.desas[0].id;
+                }
+            }
 
             let where = {};
 
--- routes/korluhPalawija.route.js
+++ routes/korluhPalawija.route.js
@@ -1,4 +1,5 @@
 const korluhPalawijaController = require('../controllers/korluhPalawija.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
@@ -6,7 +7,7 @@ const route = express.Router();
 let prefix = '/korluh/palawija';
 /* -- ROUTE -- */
 route.post(prefix + '/create', korluhPalawijaController.create);
-route.get(prefix + '/get', korluhPalawijaController.getAll);
+route.get(prefix + '/get', [mid.checkUserOrPass()], korluhPalawijaController.getAll);
 route.get(prefix + '/get/:id', korluhPalawijaController.getOne);
 route.put(prefix + '/update/:id', korluhPalawijaController.update);
 route.delete(prefix + '/delete/:id', korluhPalawijaController.delete);
