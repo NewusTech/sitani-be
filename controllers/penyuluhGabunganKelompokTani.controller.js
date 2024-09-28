1dba1405013c5d0f60354ee857547a8599cdf5ea
RozKoy <okkoy.14@gmail.com>
2024-09-28 09:08:12 +0700

    feat(endpoint): get one penyuluh gabungan kelompok tani

--- controllers/penyuluhGabunganKelompokTani.controller.js
+++ controllers/penyuluhGabunganKelompokTani.controller.js
@@ -265,4 +265,39 @@ module.exports = {
             res.status(500).json(response(500, err.message));
         }
     },
+
+    getOne: async (req, res) => {
+        try {
+            const { id } = req.params;
+
+            const penyuluhGabunganKelompokTani = await PenyuluhGabunganKelompokTani.findOne({
+                where: { id },
+                include: [
+                    {
+                        model: Kecamatan,
+                        as: 'kecamatan',
+                    },
+                    {
+                        model: Desa,
+                        as: 'desa',
+                    },
+                ],
+            });
+
+            if (!penyuluhGabunganKelompokTani) {
+                res.status(404).json(response(404, 'Penyuluh gabungan kelompok tani not found'));
+                return;
+            }
+
+            res.status(200).json(response(200, 'Get penyuluh gabungan kelompok tani successfully', penyuluhGabunganKelompokTani));
+        } catch (err) {
+            console.log(err);
+
+            logger.error(`Error : ${err}`);
+            logger.error(`Error message: ${err.message}`);
+
+            // res.status(500).json(response(500, 'Internal server error'));
+            res.status(500).json(response(500, err.message));
+        }
+    },
 }
--- routes/penyuluhGabunganKelompokTani.route.js
+++ routes/penyuluhGabunganKelompokTani.route.js
@@ -7,6 +7,7 @@ let prefix = '/penyuluh-gabungan-kelompok-tani';
 /* -- ROUTE -- */
 route.post(prefix + '/create', penyuluhGabunganKelompokTaniController.create);
 route.get(prefix + '/get', penyuluhGabunganKelompokTaniController.getAll);
+route.get(prefix + '/get/:id', penyuluhGabunganKelompokTaniController.getOne);
 /* -- ROUTE -- */
 
 module.exports = route;
