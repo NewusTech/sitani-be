30325b1330688a8824b8889de2fa9bf309197a51
RozKoy <okkoy.14@gmail.com>
2024-09-28 16:41:20 +0700

    feat(endpoint): request revalidation korluh palawija

--- routes/validasiKorluhPalawija.route.js
+++ routes/validasiKorluhPalawija.route.js
@@ -5,6 +5,7 @@ const route = express.Router();
 
 let prefix = '/validasi/korluh-palawija';
 /* -- ROUTE -- */
+route.post(prefix + '/update/:id', validasiKorluhPalawijaController.reqValidation);
 route.post(prefix + '/set', validasiKorluhPalawijaController.validate);
 route.get(prefix + '/data', validasiKorluhPalawijaController.data);
 /* -- ROUTE -- */
