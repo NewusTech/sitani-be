d99fe9a85e389470210ff05c236410f3751c2e32
RozKoy <okkoy.14@gmail.com>
2024-09-28 16:10:08 +0700

    feat(endpoint): status laporan korluh tanaman biofarmaka

--- controllers/korluhStatusLaporan.controller.js
+++ controllers/korluhStatusLaporan.controller.js
@@ -1,4 +1,6 @@
 const {
+    ValidasiKorluhTanamanBiofarmaka,
+    KorluhTanamanBiofarmaka,
     ValidasiKorluhSayurBuah,
     ValidasiKorluhPalawija,
     ValidasiKorluhPadi,
@@ -15,6 +17,16 @@ const { response } = require('../helpers');
 const { Op } = require('sequelize');
 
 const v = new Validator();
+
+const getInterval = (triwulan) => {
+    const interval = {
+        1: { start: 1, end: 3, value: 1 },
+        2: { start: 4, end: 6, value: 2 },
+        3: { start: 7, end: 9, value: 3 },
+        4: { start: 10, end: 12, value: 4 },
+    }
+    return interval[triwulan];
+}
 
 module.exports = {
     statusPadi: async (req, res) => {
@@ -70,13 +82,7 @@ module.exports = {
                     if (itemYear.tahun === tahun || !tahun) {
                         for (let m = 1; m <= 12; m++) {
                             if (m === bulan || !bulan) {
-                                const korluhPadi = await KorluhPadi.findOne({
-                                    include: [
-                                        {
-                                            model: Kecamatan,
-                                            as: 'kecamatan',
-                                        },
-                                    ],
+                                const korluhPadi = await KorluhPadi.count({
                                     where: {
                                         kecamatanId: kec.id,
                                         [Op.and]: [
@@ -186,13 +192,7 @@ module.exports = {
                     if (itemYear.tahun === tahun || !tahun) {
                         for (let m = 1; m <= 12; m++) {
                             if (m === bulan || !bulan) {
-                                const korluhPalawija = await KorluhPalawija.findOne({
-                                    include: [
-                                        {
-                                            model: Kecamatan,
-                                            as: 'kecamatan',
-                                        },
-                                    ],
+                                const korluhPalawija = await KorluhPalawija.count({
                                     where: {
                                         kecamatanId: kec.id,
                                         [Op.and]: [
@@ -302,13 +302,7 @@ module.exports = {
                     if (itemYear.tahun === tahun || !tahun) {
                         for (let m = 1; m <= 12; m++) {
                             if (m === bulan || !bulan) {
-                                const korluhSayurBuah = await KorluhSayurBuah.findOne({
-                                    include: [
-                                        {
-                                            model: Kecamatan,
-                                            as: 'kecamatan',
-                                        },
-                                    ],
+                                const korluhSayurBuah = await KorluhSayurBuah.count({
                                     where: {
                                         kecamatanId: kec.id,
                                         [Op.and]: [
@@ -354,6 +348,117 @@ module.exports = {
             }
 
             res.status(200).json(response(200, 'Get status laporan korluh sayur buah successfully', data));
+        } catch (err) {
+            console.log(err);
+
+            logger.error(`Error : ${err}`);
+            logger.error(`Error message: ${err.message}`);
+
+            // res.status(500).json(response(500, 'Internal server error'));
+            res.status(500).json(response(500, err.message));
+        }
+    },
+
+    statusBiofarmaka: async (req, res) => {
+        try {
+            let { kecamatan, triwulan, tahun, status } = req.query;
+
+            if (req?.root?.userId) {
+                const user = await User.findByPk(req.root.userId, {
+                    include: [
+                        {
+                            model: Kecamatan,
+                            as: 'kecamatans'
+                        },
+                    ]
+                });
+
+                if (user?.kecamatans?.length) {
+                    kecamatan = user.kecamatans[0].id;
+                }
+            }
+
+            kecamatan = !isNaN(parseInt(kecamatan)) ? parseInt(kecamatan) : null;
+            triwulan = !isNaN(parseInt(triwulan)) ? parseInt(triwulan) : null;
+            tahun = !isNaN(parseInt(tahun)) ? parseInt(tahun) : null;
+
+            let where = {};
+
+            if (kecamatan) {
+                where.id = kecamatan;
+            }
+
+            const kecamatanList = await Kecamatan.findAll({
+                where,
+            });
+
+            let data = {
+                ids: [],
+            };
+
+            for (let kec of kecamatanList) {
+                const years = await KorluhTanamanBiofarmaka.findAll({
+                    attributes: [
+                        [sequelize.fn('DISTINCT', sequelize.fn('YEAR', sequelize.col('tanggal'))), 'tahun'],
+                    ],
+                    order: [['tahun', 'DESC']],
+                    raw: true,
+                    where: {
+                        kecamatanId: kec.id
+                    },
+                });
+
+                for (let itemYear of years) {
+                    if (itemYear.tahun === tahun || !tahun) {
+                        for (let m = 1; m <= 4; m++) {
+                            if (m === triwulan || !triwulan) {
+                                const temp = getInterval(m);
+
+                                const korluhTanamanBiofarmaka = await KorluhTanamanBiofarmaka.count({
+                                    where: {
+                                        kecamatanId: kecamatan.id,
+                                        [Op.and]: [
+                                            sequelize.where(sequelize.fn('MONTH', sequelize.col('tanggal')), '>=', temp.start),
+                                            sequelize.where(sequelize.fn('MONTH', sequelize.col('tanggal')), '<=', temp.end),
+                                            sequelize.where(sequelize.fn('YEAR', sequelize.col('tanggal')), itemYear.tahun),
+                                        ]
+                                    }
+                                });
+
+                                if (korluhTanamanBiofarmaka) {
+                                    const validasiKorluhTanamanBiofarmaka = await ValidasiKorluhTanamanBiofarmaka.findOne({
+                                        where: {
+                                            kecamatanId: kec.id,
+                                            tahun: itemYear.tahun,
+                                            triwulan: m,
+                                        },
+                                    });
+
+                                    const obj = {
+                                        kecamatan: kec.nama,
+                                        tahun: itemYear.tahun,
+                                        triwulan: m,
+                                        status: validasiKorluhTanamanBiofarmaka?.status || 'belum',
+                                        keterangan: validasiKorluhTanamanBiofarmaka?.keterangan || null,
+                                        id: validasiKorluhTanamanBiofarmaka?.id || null,
+                                    }
+
+                                    if (status === obj.status || !status) {
+                                        if (!data.ids.includes(kec.id)) {
+                                            data.ids.push(kec.id);
+                                        }
+
+                                        data[kec.id] = data[kec.id] || [];
+                                        data[kec.id].push(obj);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            res.status(200).json(response(200, 'Get status laporan korluh tanaman biofarmaka successfully', data));
         } catch (err) {
             console.log(err);
 
--- routes/korluhStatusLaporan.route.js
+++ routes/korluhStatusLaporan.route.js
@@ -9,6 +9,7 @@ let prefix = '/status-laporan';
 route.get(prefix + '/padi', [mid.checkUserOrPass()], korluhStatusLaporanController.statusPadi);
 route.get(prefix + '/palawija', [mid.checkUserOrPass()], korluhStatusLaporanController.statusPalawija);
 route.get(prefix + '/sayur-buah', [mid.checkUserOrPass()], korluhStatusLaporanController.statusSayurBuah);
+route.get(prefix + '/biofarmaka', [mid.checkUserOrPass()], korluhStatusLaporanController.statusBiofarmaka);
 /* -- ROUTE -- */
 
 module.exports = route;
