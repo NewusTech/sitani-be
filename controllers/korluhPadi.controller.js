ad442b91720ef3bea0256751b0e3c1347c40776e
RozKoy <okkoy.14@gmail.com>
2024-09-29 19:47:15 +0700

    feat(korluh padi controller): pagination handle

--- controllers/korluhPadi.controller.js
+++ controllers/korluhPadi.controller.js
@@ -434,6 +434,9 @@ module.exports = {
                 where.kecamatanId = parseInt(kecamatan);
             }
             if (bulan || tahun) {
+                offset = undefined;
+                limit = undefined;
+
                 tahun = !isNaN(parseInt(tahun)) ? parseInt(tahun) : new Date().getFullYear();
                 bulan = !isNaN(parseInt(bulan)) ? parseInt(bulan) : new Date().getMonth() + 1;
 
@@ -483,6 +486,8 @@ module.exports = {
             });
 
             const count = await KorluhPadi.count({ where });
+
+            limit = limit || 1;
 
             const pagination = generatePagination(count, page, limit, '/api/korluh/padi/get');
 
