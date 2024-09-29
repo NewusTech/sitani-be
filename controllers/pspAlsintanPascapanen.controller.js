1365c3a8abe2fa997112986d29ea12dc5b8bb746
RozKoy <okkoy.14@gmail.com>
2024-09-29 10:59:01 +0700

    feat(endpoint): get all psp alsintan pascapanen

--- controllers/pspAlsintanPascapanen.controller.js
+++ controllers/pspAlsintanPascapanen.controller.js
@@ -253,4 +253,57 @@ module.exports = {
             res.status(500).json(response(500, err.message));
         }
     },
+
+    getAll: async (req, res) => {
+        try {
+            let { kecamatan, limit, page, year } = req.query;
+
+            limit = isNaN(parseInt(limit)) ? 10 : parseInt(limit);
+            page = isNaN(parseInt(page)) ? 1 : parseInt(page);
+
+            const offset = (page - 1) * limit;
+
+            kecamatan = isNaN(parseInt(kecamatan)) ? null : parseInt(kecamatan);
+            year = isNaN(parseInt(year)) ? null : parseInt(year);
+
+            let where = {};
+
+            if (kecamatan) {
+                where.kecamatanId = kecamatan;
+            }
+            if (year) {
+                where.tahun = year;
+            }
+
+            const pspAlsintanPascapanen = await PspAlsintanPascapanen.findAll({
+                include: [
+                    {
+                        model: Kecamatan,
+                        as: 'kecamatan',
+                    },
+                ],
+                order: [
+                    ['tahun', 'DESC'],
+                    ['createdAt', 'DESC']
+                ],
+                offset,
+                limit,
+                where,
+            });
+
+            const count = await PspAlsintanPascapanen.count({ where });
+
+            const pagination = generatePagination(count, page, limit, '/api/psp/alsintan-pascapanen/get');
+
+            res.status(200).json(response(200, 'Get PSP alsintan pascapanen successfully', { data: pspAlsintanPascapanen, pagination }));
+        } catch (err) {
+            console.log(err);
+
+            logger.error(`Error : ${err}`);
+            logger.error(`Error message: ${err.message}`);
+
+            // res.status(500).json(response(500, 'Internal server error'));
+            res.status(500).json(response(500, err.message));
+        }
+    },
 }
--- routes/pspAlsintanPascapanen.route.js
+++ routes/pspAlsintanPascapanen.route.js
@@ -6,6 +6,7 @@ const route = express.Router();
 let prefix = '/psp/alsintan-pascapanen';
 /* -- ROUTE -- */
 route.post(prefix + '/create', pspAlsintanPascapanenController.create);
+route.get(prefix + '/get', pspAlsintanPascapanenController.getAll);
 /* -- ROUTE -- */
 
 module.exports = route;
