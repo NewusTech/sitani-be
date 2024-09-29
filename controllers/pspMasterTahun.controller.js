0bee1db29afe1ae1bff0eec47c8f8617c02ca523
RozKoy <okkoy.14@gmail.com>
2024-09-29 19:21:39 +0700

    feat(endpoint): get all years psp bantuan

--- controllers/pspMasterTahun.controller.js
+++ controllers/pspMasterTahun.controller.js
@@ -1,4 +1,4 @@
-const { PspPenerimaUppo, PspPupuk, sequelize } = require('../models');
+const { PspPenerimaUppo, PspBantuan, PspPupuk, sequelize } = require('../models');
 const logger = require('../errorHandler/logger');
 const Validator = require("fastest-validator");
 const { response } = require('../helpers');
@@ -44,6 +44,30 @@ module.exports = {
             tahun = tahun.map(item => item.tahun);
 
             res.status(200).json(response(200, 'Get tahun psp pupuk successfully', tahun));
+        } catch (err) {
+            console.log(err);
+
+            logger.error(`Error : ${err}`);
+            logger.error(`Error message: ${err.message}`);
+
+            // res.status(500).json(response(500, 'Internal server error'));
+            res.status(500).json(response(500, err.message));
+        }
+    },
+
+    bantuan: async (req, res) => {
+        try {
+            let tahun = await PspBantuan.findAll({
+                attributes: [
+                    [sequelize.fn('DISTINCT', sequelize.fn('YEAR', sequelize.col('periode'))), 'tahun'],
+                ],
+                order: [['tahun', 'DESC']],
+                raw: true,
+            });
+
+            tahun = tahun.map(item => item.tahun);
+
+            res.status(200).json(response(200, 'Get tahun psp bantuan successfully', tahun));
         } catch (err) {
             console.log(err);
 
