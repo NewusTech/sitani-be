eb042b956ffbbfd135ee2c1901ea9b65490a1b48
RozKoy <okkoy.14@gmail.com>
2024-09-30 15:13:59 +0700

    feat(helper): error custom message req validation

---
+++ helpers/error.custom.message.js
@@ -0,0 +1,8 @@
+module.exports = {
+    customMessages: {
+        stringMax: "'{field}' harus kurang atau sama dengan {expected} karakter",
+        stringMin: "'{field}' harus lebih atau sama dengan {expected} karakter",
+        string: "'{field}' harus berupa string",
+        required: "'{field}' wajib diisi",
+    },
+}
--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,7 @@
 const { getFirstLastDate, dateGenerate } = require('./date.generator');
+const { customMessages } = require('./error.custom.message');
 const { jwtGenerate } = require('./token.generator');
 const { response } = require('./response.formatter');
 const { fixedNumber } = require('./number.fixed');
 
-module.exports = { getFirstLastDate, dateGenerate, jwtGenerate, fixedNumber, response };
+module.exports = { getFirstLastDate, customMessages, dateGenerate, jwtGenerate, fixedNumber, response };
