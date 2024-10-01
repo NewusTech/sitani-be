21868c473b4fa473e60efb92f431364deb407dc3
RozKoy <okkoy.14@gmail.com>
2024-10-01 13:15:26 +0700

    feat(error custom message helper): validation

--- helpers/error.custom.message.js
+++ helpers/error.custom.message.js
@@ -2,6 +2,8 @@ module.exports = {
     customMessages: {
         stringMax: "'{field}' harus kurang atau sama dengan {expected} karakter",
         stringMin: "'{field}' harus lebih atau sama dengan {expected} karakter",
+        numberMax: "'{field}' harus kurang atau sama dengan {expected}",
+        numberMin: "'{field}' harus lebih atau sama dengan {expected}",
         numberPositive: "'{field}' harus berupa bilangan positif",
         numberInteger: "'{field}' harus berupa bilangan bulat",
         number: "'{field}' harus berupa angka / numeric",
