07875271f3ecbb8474cb9aa845c9eb708590d39d
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:40:31 +0700

    feat(perkebunan dashboard route): use middleware

--- routes/perkebunanDashboard.route.js
+++ routes/perkebunanDashboard.route.js
@@ -1,11 +1,23 @@
 const perkebunanDashboardController = require('../controllers/perkebunanDashboard.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+
+	"perkebunan kabupaten",
+
+	"perkebunan kecamatan tambah",
+	"perkebunan kecamatan lihat",
+	"perkebunan kecamatan ubah",
+	"perkebunan kecamatan hapus",
+];
+
 let prefix = '/perkebunan/dashboard';
 /* -- ROUTE -- */
-route.get(prefix + '/get', perkebunanDashboardController.get);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], perkebunanDashboardController.get);
 /* -- ROUTE -- */
 
 module.exports = route;
