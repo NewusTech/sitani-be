511f1d3d7a07caa2faf43cafa0693b57caae1404
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:19:56 +0700

    feat(korluh padi route): use middleware

--- routes/korluhPadi.route.js
+++ routes/korluhPadi.route.js
@@ -4,13 +4,21 @@ const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"korluh padi tambah",
+	"korluh padi lihat",
+	"korluh padi ubah",
+	"korluh padi hapus",
+];
+
 let prefix = '/korluh/padi';
 /* -- ROUTE -- */
-route.post(prefix + '/create', korluhPadiController.create);
-route.get(prefix + '/get', [mid.checkUserOrPass()], korluhPadiController.getAll);
-route.get(prefix + '/get/:id', korluhPadiController.getOne);
-route.put(prefix + '/update/:id', korluhPadiController.update);
-route.delete(prefix + '/delete/:id', korluhPadiController.delete);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout(allowPermissions.slice(0, 2))], korluhPadiController.create);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], korluhPadiController.getAll);
+route.get(prefix + '/get/:id', [mid.checkPermissionAndLogout(allowPermissions)], korluhPadiController.getOne);
+route.put(prefix + '/update/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], korluhPadiController.update);
+route.delete(prefix + '/delete/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[4]])], korluhPadiController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
