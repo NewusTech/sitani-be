d47beb31a48f3a85dc97f673936a4d9566e5b835
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:50:08 +0700

    feat(psp bantuan route): use middleware

--- routes/pspBantuan.route.js
+++ routes/pspBantuan.route.js
@@ -1,15 +1,24 @@
 const pspBantuanController = require('../controllers/pspBantuan.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"psp bantuan tambah",
+	"psp bantuan lihat",
+	"psp bantuan ubah",
+	"psp bantuan hapus",
+];
+
 let prefix = '/psp/bantuan';
 /* -- ROUTE -- */
-route.post(prefix + '/create', pspBantuanController.create);
-route.get(prefix + '/get', pspBantuanController.getAll);
-route.get(prefix + '/get/:id', pspBantuanController.getOneById);
-route.put(prefix + '/update/:id', pspBantuanController.update);
-route.delete(prefix + '/delete/:id', pspBantuanController.delete);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout(allowPermissions.slice(0, 2))], pspBantuanController.create);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], pspBantuanController.getAll);
+route.get(prefix + '/get/:id', [mid.checkPermissionAndLogout(allowPermissions)], pspBantuanController.getOneById);
+route.put(prefix + '/update/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], pspBantuanController.update);
+route.delete(prefix + '/delete/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[4]])], pspBantuanController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
