b8f646e33b65eb3d2708f56b5f06af425180a3fc
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:44:52 +0700

    feat(psp alsintan route): use middleware

--- routes/pspAlsintan.route.js
+++ routes/pspAlsintan.route.js
@@ -1,11 +1,17 @@
 const pspAlsintanController = require('../controllers/pspAlsintan.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"psp alsintan",
+];
+
 let prefix = '/psp/alsintan/';
 /* -- ROUTE -- */
-route.get(prefix + '/kecamatan', pspAlsintanController.getKecamatan);
+route.get(prefix + '/kecamatan', [mid.checkPermissionAndLogout(allowPermissions)], pspAlsintanController.getKecamatan);
 /* -- ROUTE -- */
 
 module.exports = route;
