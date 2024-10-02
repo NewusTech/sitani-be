36a81e3874c6585d2a208cd529b6e1dfd33cf4b4
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:53:18 +0700

    feat(psp penerima uppo route): use middleware

--- routes/pspPenerimaUppo.route.js
+++ routes/pspPenerimaUppo.route.js
@@ -1,15 +1,24 @@
 const pspPenerimaUppoController = require('../controllers/pspPenerimaUppo.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"psp uppo tambah",
+	"psp uppo lihat",
+	"psp uppo ubah",
+	"psp uppo hapus",
+];
+
 let prefix = '/psp/penerima-uppo';
 /* -- ROUTE -- */
-route.post(prefix + '/create', pspPenerimaUppoController.create);
-route.get(prefix + '/get', pspPenerimaUppoController.getAll);
-route.get(prefix + '/get/:id', pspPenerimaUppoController.getOneById);
-route.put(prefix + '/update/:id', pspPenerimaUppoController.update);
-route.delete(prefix + '/delete/:id', pspPenerimaUppoController.delete);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout(allowPermissions.slice(0, 2))], pspPenerimaUppoController.create);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], pspPenerimaUppoController.getAll);
+route.get(prefix + '/get/:id', [mid.checkPermissionAndLogout(allowPermissions)], pspPenerimaUppoController.getOneById);
+route.put(prefix + '/update/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], pspPenerimaUppoController.update);
+route.delete(prefix + '/delete/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[4]])], pspPenerimaUppoController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
