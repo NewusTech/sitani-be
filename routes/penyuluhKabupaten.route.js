714d9ce65f91de4ac50f41c8f64dd86c7e51510b
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:35:14 +0700

    feat(penyuluh kabupaten route): use middleware

--- routes/penyuluhKabupaten.route.js
+++ routes/penyuluhKabupaten.route.js
@@ -1,15 +1,24 @@
 const penyuluhKabupatenController = require('../controllers/penyuluhKabupaten.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"penyuluh kabupaten tambah",
+	"penyuluh kabupaten lihat",
+	"penyuluh kabupaten ubah",
+	"penyuluh kabupaten hapus",
+];
+
 let prefix = '/penyuluh-kabupaten';
 /* -- ROUTE -- */
-route.post(prefix + '/create', penyuluhKabupatenController.create);
-route.get(prefix + '/get', penyuluhKabupatenController.getAll);
-route.get(prefix + '/get/:id', penyuluhKabupatenController.getOneById);
-route.put(prefix + '/update/:id', penyuluhKabupatenController.update);
-route.delete(prefix + '/delete/:id', penyuluhKabupatenController.delete);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout(allowPermissions.slice(0, 2))], penyuluhKabupatenController.create);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], penyuluhKabupatenController.getAll);
+route.get(prefix + '/get/:id', [mid.checkPermissionAndLogout(allowPermissions)], penyuluhKabupatenController.getOneById);
+route.put(prefix + '/update/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], penyuluhKabupatenController.update);
+route.delete(prefix + '/delete/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[4]])], penyuluhKabupatenController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
