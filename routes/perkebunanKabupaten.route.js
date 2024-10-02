a2ff099b5119de21eac4c43992ce506f9f20da0f
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:41:46 +0700

    feat(perkebunan kabupaten route): use middleware

--- routes/perkebunanKabupaten.route.js
+++ routes/perkebunanKabupaten.route.js
@@ -1,11 +1,17 @@
 const perkebunanKabupatenController = require('../controllers/perkebunanKabupaten.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"perkebunan kabupaten",
+];
+
 let prefix = '/perkebunan/kabupaten';
 /* -- ROUTE -- */
-route.get(prefix + '/get', perkebunanKabupatenController.getAll);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], perkebunanKabupatenController.getAll);
 /* -- ROUTE -- */
 
 module.exports = route;
