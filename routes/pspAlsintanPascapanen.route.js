d6cc97a4926cbe40a8872ffa1d3d87cc56827a5d
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:46:41 +0700

    feat(psp alsintan pascapanen route): use middleware

--- routes/pspAlsintanPascapanen.route.js
+++ routes/pspAlsintanPascapanen.route.js
@@ -1,15 +1,24 @@
 const pspAlsintanPascapanenController = require('../controllers/pspAlsintanPascapanen.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"psp alsintan pascapanen tambah",
+	"psp alsintan pascapanen lihat",
+	"psp alsintan pascapanen ubah",
+	"psp alsintan pascapanen hapus",
+];
+
 let prefix = '/psp/alsintan-pascapanen';
 /* -- ROUTE -- */
-route.post(prefix + '/create', pspAlsintanPascapanenController.create);
-route.get(prefix + '/get', pspAlsintanPascapanenController.getAll);
-route.get(prefix + '/get/:id', pspAlsintanPascapanenController.getOne);
-route.put(prefix + '/update/:id', pspAlsintanPascapanenController.update);
-route.delete(prefix + '/delete/:id', pspAlsintanPascapanenController.delete);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout(allowPermissions.slice(0, 2))], pspAlsintanPascapanenController.create);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], pspAlsintanPascapanenController.getAll);
+route.get(prefix + '/get/:id', [mid.checkPermissionAndLogout(allowPermissions)], pspAlsintanPascapanenController.getOne);
+route.put(prefix + '/update/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], pspAlsintanPascapanenController.update);
+route.delete(prefix + '/delete/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[4]])], pspAlsintanPascapanenController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
