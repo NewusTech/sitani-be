9416623f7075593a1e8226f797c85122bd2a95f6
RozKoy <okkoy.14@gmail.com>
2024-10-02 12:14:31 +0700

    feat(kepang perbandingan harga route): use middleware

--- routes/kepangPerbandinganHarga.route.js
+++ routes/kepangPerbandinganHarga.route.js
@@ -1,11 +1,17 @@
 const kepangPerbandinganHargaController = require('../controllers/kepangPerbandinganHarga.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"kepang perbandingan harga",
+];
+
 let prefix = '/kepang/perbandingan-harga';
 /* -- ROUTE -- */
-route.get(prefix + '/get', kepangPerbandinganHargaController.getAll);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], kepangPerbandinganHargaController.getAll);
 /* -- ROUTE -- */
 
 module.exports = route;
