c89c75ecbe9ae7557ef030b4fecb9ac5ca5bff90
RozKoy <okkoy.14@gmail.com>
2024-10-02 15:10:38 +0700

    feat(validasi korluh tanaman hias route): use middleware

--- routes/validasiKorluhTanamanHias.route.js
+++ routes/validasiKorluhTanamanHias.route.js
@@ -1,13 +1,19 @@
 const validasiKorluhTanamanHiasController = require('../controllers/validasiKorluhTanamanHias.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"tph validasi korluh tanaman hias",
+];
+
 let prefix = '/validasi/korluh-tanaman-hias';
 /* -- ROUTE -- */
-route.post(prefix + '/update/:id', validasiKorluhTanamanHiasController.reqValidation);
-route.post(prefix + '/set', validasiKorluhTanamanHiasController.validate);
-route.get(prefix + '/data', validasiKorluhTanamanHiasController.data);
+route.post(prefix + '/update/:id', [mid.checkPermissionAndLogout(allowPermissions)], validasiKorluhTanamanHiasController.reqValidation);
+route.post(prefix + '/set', [mid.checkPermissionAndLogout(allowPermissions)], validasiKorluhTanamanHiasController.validate);
+route.get(prefix + '/data', [mid.checkPermissionAndLogout(allowPermissions)], validasiKorluhTanamanHiasController.data);
 /* -- ROUTE -- */
 
 module.exports = route;
