29a38f33d6145b3219e51706c7475cb57d3c889c
RozKoy <okkoy.14@gmail.com>
2024-10-02 15:00:19 +0700

    feat(tph lahan sawah route): use middleware

--- routes/tphLahanSawah.route.js
+++ routes/tphLahanSawah.route.js
@@ -1,15 +1,24 @@
 const tphLahanSawahController = require('../controllers/tphLahanSawah.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"tph lahan sawah tambah",
+	"tph lahan sawah lihat",
+	"tph lahan sawah ubah",
+	"tph lahan sawah hapus",
+];
+
 let prefix = '/tph/lahan-sawah';
 /* -- ROUTE -- */
-route.post(prefix + '/create', tphLahanSawahController.create);
-route.get(prefix + '/get', tphLahanSawahController.getAll);
-route.get(prefix + '/get/:id', tphLahanSawahController.getOne);
-route.put(prefix + '/update/:id', tphLahanSawahController.update);
-route.delete(prefix + '/delete/:id', tphLahanSawahController.delete);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout(allowPermissions.slice(0, 2))], tphLahanSawahController.create);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], tphLahanSawahController.getAll);
+route.get(prefix + '/get/:id', [mid.checkPermissionAndLogout(allowPermissions)], tphLahanSawahController.getOne);
+route.put(prefix + '/update/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], tphLahanSawahController.update);
+route.delete(prefix + '/delete/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[4]])], tphLahanSawahController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
