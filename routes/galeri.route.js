33e5f87d7877e8029ed84b2c6993fe5b827f4048
RozKoy <okkoy.14@gmail.com>
2024-10-02 12:00:01 +0700

    feat(galeri route): use middleware

--- routes/galeri.route.js
+++ routes/galeri.route.js
@@ -1,4 +1,5 @@
 const galeriController = require('../controllers/galeri.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 const multer = require('multer');
 
@@ -7,13 +8,21 @@ const route = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+const allowPermissions = [
+	"semua",
+	"galeri tambah",
+	"galeri lihat",
+	"galeri ubah",
+	"galeri hapus",
+];
+
 let prefix = '/galeri';
 /* -- ROUTE -- */
-route.post(prefix + '/create', upload.fields([{ name: 'image', maxCount: 1 }]), galeriController.create);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout([allowPermissions.slice(0, 2)])], upload.fields([{ name: 'image', maxCount: 1 }]), galeriController.create);
 route.get(prefix + '/get', galeriController.getAll);
 route.get(prefix + '/get/:id', galeriController.getOneById);
-route.put(prefix + '/update/:id', upload.fields([{ name: 'image', maxCount: 1 }]), galeriController.update);
-route.delete(prefix + '/delete/:id', galeriController.delete);
+route.put(prefix + '/update/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], upload.fields([{ name: 'image', maxCount: 1 }]), galeriController.update);
+route.delete(prefix + '/delete/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[4]])], galeriController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
