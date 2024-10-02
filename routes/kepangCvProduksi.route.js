2599d30662c053889326bc30db02dd37b340625c
RozKoy <okkoy.14@gmail.com>
2024-10-02 12:03:50 +0700

    feat(kepang cv produksi route): use middleware

--- routes/kepangCvProduksi.route.js
+++ routes/kepangCvProduksi.route.js
@@ -1,15 +1,24 @@
 const kepangCvProduksiController = require('../controllers/kepangCvProduksi.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"kepang cv produksi tambah",
+	"kepang cv produksi lihat",
+	"kepang cv produksi ubah",
+	"kepang cv produksi hapus",
+];
+
 let prefix = '/kepang/cv-produksi';
 /* -- ROUTE -- */
-route.post(prefix + '/create', kepangCvProduksiController.create);
-route.get(prefix + '/get', kepangCvProduksiController.getAll);
-route.get(prefix + '/get/:id', kepangCvProduksiController.getOne);
-route.put(prefix + '/update/:id', kepangCvProduksiController.update);
-route.delete(prefix + '/delete/:id', kepangCvProduksiController.delete);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout(allowPermissions.slice(0, 2))], kepangCvProduksiController.create);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], kepangCvProduksiController.getAll);
+route.get(prefix + '/get/:id', [mid.checkPermissionAndLogout(allowPermissions)], kepangCvProduksiController.getOne);
+route.put(prefix + '/update/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], kepangCvProduksiController.update);
+route.delete(prefix + '/delete/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[4]])], kepangCvProduksiController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
