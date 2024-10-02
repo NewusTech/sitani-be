a4efa680d820e7779a0d6802169366c4b3d89ae2
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:51:40 +0700

    feat(psp dashboard route): use middleware

--- routes/pspDashboard.route.js
+++ routes/pspDashboard.route.js
@@ -1,11 +1,43 @@
 const pspDashboardController = require('../controllers/pspDashboard.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+
+	"psp alsintan",
+
+	"psp alsintan prapanen tambah",
+	"psp alsintan prapanen lihat",
+	"psp alsintan prapanen ubah",
+	"psp alsintan prapanen hapus",
+
+	"psp alsintan pascapanen tambah",
+	"psp alsintan pascapanen lihat",
+	"psp alsintan pascapanen ubah",
+	"psp alsintan pascapanen hapus",
+
+	"psp uppo tambah",
+	"psp uppo lihat",
+	"psp uppo ubah",
+	"psp uppo hapus",
+
+	"psp pupuk tambah",
+	"psp pupuk lihat",
+	"psp pupuk ubah",
+	"psp pupuk hapus",
+
+	"psp bantuan tambah",
+	"psp bantuan lihat",
+	"psp bantuan ubah",
+	"psp bantuan hapus",
+];
+
 let prefix = '/psp/dashboard';
 /* -- ROUTE -- */
-route.get(prefix + '/get', pspDashboardController.get);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], pspDashboardController.get);
 /* -- ROUTE -- */
 
 module.exports = route;
