04544c176dca413a545351924683d149d31961a6
RozKoy <okkoy.14@gmail.com>
2024-10-02 11:41:01 +0700

    feat(helper): permissions list

---
+++ helpers/permissions.list.js
@@ -0,0 +1,171 @@
+module.exports = {
+	PERMISSIONS: [
+		"semua",
+
+		// KELOLA PENGGUNA
+		"pengguna tambah",
+		"pengguna lihat",
+		"pengguna ubah",
+		"pengguna hapus",
+
+		// KELOLA BERITA
+		"berita tambah",
+		"berita lihat",
+		"berita ubah",
+		"berita hapus",
+
+		// KELOLA GALERI
+		"galeri tambah",
+		"galeri lihat",
+		"galeri ubah",
+		"galeri hapus",
+
+		// KELOLA KETAHANAN PANGAN PERBANDINGAN HARGA
+		"kepang perbandingan harga",
+		// KELOLA KETAHANAN PANGAN CV PRODUKSI
+		"kepang cv produksi tambah",
+		"kepang cv produksi lihat",
+		"kepang cv produksi ubah",
+		"kepang cv produksi hapus",
+		// KELOLA KETAHANAN PANGAN CV PRODUSEN
+		"kepang cv produsen tambah",
+		"kepang cv produsen lihat",
+		"kepang cv produsen ubah",
+		"kepang cv produsen hapus",
+		// KELOLA KETAHANAN PANGAN PEDAGANG ECERAN
+		"kepang pedagang eceran tambah",
+		"kepang pedagang eceran lihat",
+		"kepang pedagang eceran ubah",
+		"kepang pedagang eceran hapus",
+		// KELOLA KETAHANAN PANGAN PRODUSEN ECERAN
+		"kepang produsen eceran tambah",
+		"kepang produsen eceran lihat",
+		"kepang produsen eceran ubah",
+		"kepang produsen eceran hapus",
+
+		// KELOLA KEPEGAWAIAN
+		"kepegawaian tambah",
+		"kepegawaian lihat",
+		"kepegawaian ubah",
+		"kepegawaian hapus",
+
+		// KELOLA KORLUH PADI
+		"korluh padi tambah",
+		"korluh padi lihat",
+		"korluh padi ubah",
+		"korluh padi hapus",
+		// KELOLA KORLUH PALAWIJA
+		"korluh palawija tambah",
+		"korluh palawija lihat",
+		"korluh palawija ubah",
+		"korluh palawija hapus",
+		// KELOLA KORLUH SAYUR BUAH
+		"korluh sayur buah tambah",
+		"korluh sayur buah lihat",
+		"korluh sayur buah ubah",
+		"korluh sayur buah hapus",
+		// KELOLA KORLUH TANAMAN BIOFARMAKA
+		"korluh tanaman biofarmaka tambah",
+		"korluh tanaman biofarmaka lihat",
+		"korluh tanaman biofarmaka ubah",
+		"korluh tanaman biofarmaka hapus",
+		// KELOLA KORLUH TANAMAN HIAS
+		"korluh tanaman hias tambah",
+		"korluh tanaman hias lihat",
+		"korluh tanaman hias ubah",
+		"korluh tanaman hias hapus",
+
+		// KELOLA PENYULUH KABUPATEN
+		"penyuluh kabupaten tambah",
+		"penyuluh kabupaten lihat",
+		"penyuluh kabupaten ubah",
+		"penyuluh kabupaten hapus",
+		// KELOLA PENYULUH KECAMATAN
+		"penyuluh kecamatan tambah",
+		"penyuluh kecamatan lihat",
+		"penyuluh kecamatan ubah",
+		"penyuluh kecamatan hapus",
+		// KELOLA PENYULUH GABUNGAN KELOMPOK TANI
+		"penyuluh gapoktan tambah",
+		"penyuluh gapoktan lihat",
+		"penyuluh gapoktan ubah",
+		"penyuluh gapoktan hapus",
+		// KELOLA PENYULUH KELOMPOK TANI
+		"penyuluh poktan tambah",
+		"penyuluh poktan lihat",
+		"penyuluh poktan ubah",
+		"penyuluh poktan hapus",
+
+		// KELOLA PERKEBUNAN KABUPATEN
+		"perkebunan kabupaten",
+		// KELOLA PERKEBUNAN KECAMATAN
+		"perkebunan kecamatan tambah",
+		"perkebunan kecamatan lihat",
+		"perkebunan kecamatan ubah",
+		"perkebunan kecamatan hapus",
+
+		// KELOLA PSP ALSINTAN
+		"psp alsintan",
+		// KELOLA PSP ALSINTAN PRAPANEN
+		"psp alsintan prapanen tambah",
+		"psp alsintan prapanen lihat",
+		"psp alsintan prapanen ubah",
+		"psp alsintan prapanen hapus",
+		// KELOLA PSP ALSINTAN PASCAPANEN
+		"psp alsintan pascapanen tambah",
+		"psp alsintan pascapanen lihat",
+		"psp alsintan pascapanen ubah",
+		"psp alsintan pascapanen hapus",
+		// KELOLA PSP PENERIMA UPPO
+		"psp uppo tambah",
+		"psp uppo lihat",
+		"psp uppo ubah",
+		"psp uppo hapus",
+		// KELOLA PSP PUPUK
+		"psp pupuk tambah",
+		"psp pupuk lihat",
+		"psp pupuk ubah",
+		"psp pupuk hapus",
+		// KELOLA PSP BANTUAN
+		"psp bantuan tambah",
+		"psp bantuan lihat",
+		"psp bantuan ubah",
+		"psp bantuan hapus",
+
+		// KELOLA TPH REALISASI PADI
+		"tph realisasi padi tambah",
+		"tph realisasi padi lihat",
+		"tph realisasi padi ubah",
+		"tph realisasi padi hapus",
+		// KELOLA TPH REALISASI PALAWIJA I
+		"tph realisasi palawija 1 tambah",
+		"tph realisasi palawija 1 lihat",
+		"tph realisasi palawija 1 ubah",
+		"tph realisasi palawija 1 hapus",
+		// KELOLA TPH REALISASI PALAWIJA II
+		"tph realisasi palawija 2 tambah",
+		"tph realisasi palawija 2 lihat",
+		"tph realisasi palawija 2 ubah",
+		"tph realisasi palawija 2 hapus",
+		// KELOLA TPH LAHAN SAWAH
+		"tph lahan sawah tambah",
+		"tph lahan sawah lihat",
+		"tph lahan sawah ubah",
+		"tph lahan sawah hapus",
+		// KELOLA TPH LAHAN BUKAN SAWAH
+		"tph lahan bukan sawah tambah",
+		"tph lahan bukan sawah lihat",
+		"tph lahan bukan sawah ubah",
+		"tph lahan bukan sawah hapus",
+		// KELOLA TPH VALIDASI KORLUH PADI
+		"tph validasi korluh padi",
+		// KELOLA TPH VALIDASI KORLUH PALAWIJA
+		"tph validasi korluh palawija",
+		// KELOLA TPH VALIDASI KORLUH SAYUR BUAH
+		"tph validasi korluh sayur buah",
+		// KELOLA TPH VALIDASI KORLUH TANAMAN BIOFARMAKA
+		"tph validasi korluh tanaman biofarmaka",
+		// KELOLA TPH VALIDASI KORLUH TANAMAN HIAS
+		"tph validasi korluh tanaman hias",
+	],
+}
