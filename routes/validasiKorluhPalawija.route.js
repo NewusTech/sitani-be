1e0ac345fc617e607fc8a926adc2f928c38ae758
RozKoy <okkoy.14@gmail.com>
2024-10-02 15:07:21 +0700

    feat(validasi korluh palawija route): use middleware

--- routes/validasiKorluhPalawija.route.js
+++ routes/validasiKorluhPalawija.route.js
@@ -1,13 +1,19 @@
 const validasiKorluhPalawijaController = require('../controllers/validasiKorluhPalawija.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"tph validasi korluh palawija",
+];
+
 let prefix = '/validasi/korluh-palawija';
 /* -- ROUTE -- */
-route.post(prefix + '/update/:id', validasiKorluhPalawijaController.reqValidation);
-route.post(prefix + '/set', validasiKorluhPalawijaController.validate);
-route.get(prefix + '/data', validasiKorluhPalawijaController.data);
+route.post(prefix + '/update/:id', [mid.checkPermissionAndLogout(allowPermissions)], validasiKorluhPalawijaController.reqValidation);
+route.post(prefix + '/set', [mid.checkPermissionAndLogout(allowPermissions)], validasiKorluhPalawijaController.validate);
+route.get(prefix + '/data', [mid.checkPermissionAndLogout(allowPermissions)], validasiKorluhPalawijaController.data);
 /* -- ROUTE -- */
 
 module.exports = route;
