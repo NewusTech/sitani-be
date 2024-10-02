bb707cc57ff7f087674d261f8f597fd154269cee
RozKoy <okkoy.14@gmail.com>
2024-10-02 15:09:28 +0700

    feat(validasi korluh tanaman biofarmaka route): use middleware

--- routes/validasiKorluhTanamanBiofarmaka.route.js
+++ routes/validasiKorluhTanamanBiofarmaka.route.js
@@ -1,13 +1,19 @@
 const validasiKorluhTanamanBiofarmakaController = require('../controllers/validasiKorluhTanamanBiofarmaka.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"tph validasi korluh tanaman biofarmaka",
+];
+
 let prefix = '/validasi/korluh-tanaman-biofarmaka';
 /* -- ROUTE -- */
-route.post(prefix + '/update/:id', validasiKorluhTanamanBiofarmakaController.reqValidation);
-route.post(prefix + '/set', validasiKorluhTanamanBiofarmakaController.validate);
-route.get(prefix + '/data', validasiKorluhTanamanBiofarmakaController.data);
+route.post(prefix + '/update/:id', [mid.checkPermissionAndLogout(allowPermissions)], validasiKorluhTanamanBiofarmakaController.reqValidation);
+route.post(prefix + '/set', [mid.checkPermissionAndLogout(allowPermissions)], validasiKorluhTanamanBiofarmakaController.validate);
+route.get(prefix + '/data', [mid.checkPermissionAndLogout(allowPermissions)], validasiKorluhTanamanBiofarmakaController.data);
 /* -- ROUTE -- */
 
 module.exports = route;
