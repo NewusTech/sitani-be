ed5edb1c766bb153326c3b8aade51f01cd15949c
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:29:25 +0700

    feat(korluh tanaman hias route): use middleware

--- routes/korluhTanamanHias.route.js
+++ routes/korluhTanamanHias.route.js
@@ -4,13 +4,21 @@ const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"korluh tanaman hias tambah",
+	"korluh tanaman hias lihat",
+	"korluh tanaman hias ubah",
+	"korluh tanaman hias hapus",
+];
+
 let prefix = '/korluh/tanaman-hias';
 /* -- ROUTE -- */
-route.post(prefix + '/create', korluhTanamanHiasController.create);
-route.get(prefix + '/get', [mid.checkUserOrPass()], korluhTanamanHiasController.getAll);
-route.get(prefix + '/get/:id', korluhTanamanHiasController.getOne);
-route.put(prefix + '/update/:id', korluhTanamanHiasController.update);
-route.delete(prefix + '/delete/:id', korluhTanamanHiasController.delete);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout(allowPermissions.slice(0, 2))], korluhTanamanHiasController.create);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], korluhTanamanHiasController.getAll);
+route.get(prefix + '/get/:id', [mid.checkPermissionAndLogout(allowPermissions)], korluhTanamanHiasController.getOne);
+route.put(prefix + '/update/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], korluhTanamanHiasController.update);
+route.delete(prefix + '/delete/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[4]])], korluhTanamanHiasController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
