bf4d85c4d20f7e45aa4f55f93a691161436750d4
RozKoy <okkoy.14@gmail.com>
2024-10-02 12:12:54 +0700

    feat(kepang pedagang eceran route): use middleware

--- routes/kepangPedagangEceran.route.js
+++ routes/kepangPedagangEceran.route.js
@@ -1,15 +1,24 @@
 const kepangPedagangEceranController = require('../controllers/kepangPedagangEceran.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"kepang pedagang eceran tambah",
+	"kepang pedagang eceran lihat",
+	"kepang pedagang eceran ubah",
+	"kepang pedagang eceran hapus",
+];
+
 let prefix = '/kepang/pedagang-eceran';
 /* -- ROUTE -- */
-route.post(prefix + '/create', kepangPedagangEceranController.create);
-route.get(prefix + '/get', kepangPedagangEceranController.getAll);
-route.get(prefix + '/get/:id', kepangPedagangEceranController.getOne);
-route.put(prefix + '/update/:id', kepangPedagangEceranController.update);
-route.delete(prefix + '/delete/:id', kepangPedagangEceranController.delete);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout(allowPermissions.slice(0, 2))], kepangPedagangEceranController.create);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], kepangPedagangEceranController.getAll);
+route.get(prefix + '/get/:id', [mid.checkPermissionAndLogout(allowPermissions)], kepangPedagangEceranController.getOne);
+route.put(prefix + '/update/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], kepangPedagangEceranController.update);
+route.delete(prefix + '/delete/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[4]])], kepangPedagangEceranController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
