9a528b15fa1d6e1d28305417177dba6d4f445407
RozKoy <okkoy.14@gmail.com>
2024-10-02 11:53:19 +0700

    feat(article route): use middleware

--- routes/article.route.js
+++ routes/article.route.js
@@ -1,4 +1,5 @@
 const articleController = require('../controllers/article.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 const multer = require('multer');
 
@@ -7,13 +8,21 @@ const route = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+const allowPermissions = [
+	"semua",
+	"berita tambah",
+	"berita lihat",
+	"berita ubah",
+	"berita hapus",
+];
+
 let prefix = '/article';
 /* -- ROUTE -- */
-route.post(prefix + '/create', upload.fields([{ name: 'image', maxCount: 1 }]), articleController.create);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout([allowPermissions.slice(0, 2)])], upload.fields([{ name: 'image', maxCount: 1 }]), articleController.create);
 route.get(prefix + '/get', articleController.getAll);
 route.get(prefix + '/get/:slug', articleController.getOneBySlug);
-route.put(prefix + '/update/:slug', upload.fields([{ name: 'image', maxCount: 1 }]), articleController.update);
-route.delete(prefix + '/delete/:slug', articleController.delete);
+route.put(prefix + '/update/:slug', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], upload.fields([{ name: 'image', maxCount: 1 }]), articleController.update);
+route.delete(prefix + '/delete/:slug', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[4]])], articleController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
