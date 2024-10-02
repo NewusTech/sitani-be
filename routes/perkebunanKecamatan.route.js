1ec1c593e141258528c63c2a37870e50be88fa9d
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:43:35 +0700

    feat(perkebunan kecamatan route): use middleware

--- routes/perkebunanKecamatan.route.js
+++ routes/perkebunanKecamatan.route.js
@@ -1,15 +1,24 @@
 const perkebunanKecamatanController = require('../controllers/perkebunanKecamatan.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"perkebunan kecamatan tambah",
+	"perkebunan kecamatan lihat",
+	"perkebunan kecamatan ubah",
+	"perkebunan kecamatan hapus",
+];
+
 let prefix = '/perkebunan/kecamatan';
 /* -- ROUTE -- */
-route.post(prefix + '/create', perkebunanKecamatanController.create);
-route.get(prefix + '/get', perkebunanKecamatanController.getAll);
-route.get(prefix + '/get/:id', perkebunanKecamatanController.getOne);
-route.put(prefix + '/update/:id', perkebunanKecamatanController.update);
-route.delete(prefix + '/delete/:id', perkebunanKecamatanController.delete);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout(allowPermissions.slice(0, 2))], perkebunanKecamatanController.create);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], perkebunanKecamatanController.getAll);
+route.get(prefix + '/get/:id', [mid.checkPermissionAndLogout(allowPermissions)], perkebunanKecamatanController.getOne);
+route.put(prefix + '/update/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], perkebunanKecamatanController.update);
+route.delete(prefix + '/delete/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], perkebunanKecamatanController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
