18f0492315da2a8c30826aa955ac71040241b2d9
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:27:42 +0700

    feat(korluh tanaman biofarmaka route): use middleware

--- routes/korluhTanamanBiofarmaka.route.js
+++ routes/korluhTanamanBiofarmaka.route.js
@@ -4,13 +4,21 @@ const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+	"korluh tanaman biofarmaka tambah",
+	"korluh tanaman biofarmaka lihat",
+	"korluh tanaman biofarmaka ubah",
+	"korluh tanaman biofarmaka hapus",
+];
+
 let prefix = '/korluh/tanaman-biofarmaka';
 /* -- ROUTE -- */
-route.post(prefix + '/create', korluhTanamanBiofarmakaController.create);
-route.get(prefix + '/get', [mid.checkUserOrPass()], korluhTanamanBiofarmakaController.getAll);
-route.get(prefix + '/get/:id', korluhTanamanBiofarmakaController.getOne);
-route.put(prefix + '/update/:id', korluhTanamanBiofarmakaController.update);
-route.delete(prefix + '/delete/:id', korluhTanamanBiofarmakaController.delete);
+route.post(prefix + '/create', [mid.checkPermissionAndLogout(allowPermissions.slice(0, 2))], korluhTanamanBiofarmakaController.create);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], korluhTanamanBiofarmakaController.getAll);
+route.get(prefix + '/get/:id', [mid.checkPermissionAndLogout(allowPermissions)], korluhTanamanBiofarmakaController.getOne);
+route.put(prefix + '/update/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[3]])], korluhTanamanBiofarmakaController.update);
+route.delete(prefix + '/delete/:id', [mid.checkPermissionAndLogout([allowPermissions[0], allowPermissions[4]])], korluhTanamanBiofarmakaController.delete);
 /* -- ROUTE -- */
 
 module.exports = route;
