d6dacf95ce255f2305e74521759ab0ca858034f6
RozKoy <okkoy.14@gmail.com>
2024-10-02 14:31:14 +0700

    feat(penyuluh dashboard route): use middleware

--- routes/penyuluhDashboard.route.js
+++ routes/penyuluhDashboard.route.js
@@ -1,11 +1,36 @@
 const penyuluhDashboardController = require('../controllers/penyuluhDashboard.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
+const allowPermissions = [
+	"semua",
+
+	"penyuluh kabupaten tambah",
+	"penyuluh kabupaten lihat",
+	"penyuluh kabupaten ubah",
+	"penyuluh kabupaten hapus",
+
+	"penyuluh kecamatan tambah",
+	"penyuluh kecamatan lihat",
+	"penyuluh kecamatan ubah",
+	"penyuluh kecamatan hapus",
+
+	"penyuluh gapoktan tambah",
+	"penyuluh gapoktan lihat",
+	"penyuluh gapoktan ubah",
+	"penyuluh gapoktan hapus",
+
+	"penyuluh poktan tambah",
+	"penyuluh poktan lihat",
+	"penyuluh poktan ubah",
+	"penyuluh poktan hapus",
+];
+
 let prefix = '/penyuluh/dashboard';
 /* -- ROUTE -- */
-route.get(prefix + '/get', penyuluhDashboardController.get);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout(allowPermissions)], penyuluhDashboardController.get);
 /* -- ROUTE -- */
 
 module.exports = route;
