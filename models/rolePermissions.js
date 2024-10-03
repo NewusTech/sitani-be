9724619ab3603b6001c4c13fe3df7eff7e5027a9
RozKoy <okkoy.14@gmail.com>
2024-10-03 13:21:15 +0700

    feat(model): role permissions

---
+++ models/rolePermissions.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class RolePermissions extends Model {
+        static associate(models) {
+            RolePermissions.belongsTo(models.Role, {
+                foreignKey: 'roleId',
+                as: 'role',
+            });
+            RolePermissions.belongsTo(models.Permission, {
+                foreignKey: 'permissionId',
+                as: 'permission',
+            });
+        }
+    }
+
+    RolePermissions.init({
+        roleId: {
+            type: DataTypes.BIGINT,
+            field: 'role_id',
+        },
+        permissionId: {
+            type: DataTypes.BIGINT,
+            field: 'permission_id',
+        },
+    }, {
+        tableName: 'role_permissions',
+        modelName: 'RolePermissions',
+        updatedAt: false,
+        deletedAt: false,
+        sequelize,
+    });
+
+    return RolePermissions;
+};
