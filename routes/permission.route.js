2d5b3e63c1de8ef55101bf860d74fb4dcf1a2e24
RozKoy <okkoy.14@gmail.com>
2024-10-03 13:01:58 +0700

    feat(endpoint): get all permissions with middleware

--- routes/permission.route.js
+++ routes/permission.route.js
@@ -1,15 +1,12 @@
 const permissionController = require('../controllers/permission.controller');
+const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 
 const route = express.Router();
 
 let prefix = '/permission';
 /* -- ROUTE -- */
-route.post(prefix + '/create', permissionController.create);
-route.get(prefix + '/get', permissionController.getAll);
-route.get(prefix + '/get/:id', permissionController.getOneById);
-route.put(prefix + '/update/:id', permissionController.update);
-route.delete(prefix + '/delete/:id', permissionController.delete);
+route.get(prefix + '/get', [mid.checkPermissionAndLogout([])], permissionController.getAll);
 /* -- ROUTE -- */
 
 module.exports = route;
