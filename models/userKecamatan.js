c7ab1add9505ce4c7c62ae78a9960b813d1c0800
RozKoy <okkoy.14@gmail.com>
2024-10-03 14:04:25 +0700

    feat(model): user kecamatan

---
+++ models/userKecamatan.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class UserKecamatan extends Model {
+        static associate(models) {
+            UserKecamatan.belongsTo(models.Kecamatan, {
+                foreignKey: 'kecamatanId',
+                as: 'kecamatan',
+            });
+            UserKecamatan.belongsTo(models.User, {
+                foreignKey: 'userId',
+                as: 'user',
+            });
+        }
+    }
+
+    UserKecamatan.init({
+        kecamatanId: {
+            type: DataTypes.BIGINT,
+            field: 'kecamatan_id',
+        },
+        userId: {
+            type: DataTypes.BIGINT,
+            field: 'user_id',
+        },
+    }, {
+        tableName: 'user_kecamatan',
+        modelName: 'UserKecamatan',
+        updatedAt: false,
+        deletedAt: false,
+        sequelize,
+    });
+
+    return UserKecamatan;
+};
